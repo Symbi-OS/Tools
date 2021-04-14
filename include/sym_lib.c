7f06ae13e31f3d7e15cbb60625a65dbc17058b36
root <root@localhost.localdomain>
2021-04-14 17:42:42 -0400

    Add stack pushing to static lib

--- include/sym_lib.c
+++ include/sym_lib.c
@@ -1,7 +1,36 @@
-void sym_elevate();
-void sym_lower();
+#include <stdio.h>
+
+// void sym_elevate();
+// void sym_lower();
+
+void touch_stack(){
+  /* register int    syscall_no  asm("rax") = 1; // write */
+  /* register int    arg1        asm("rdi") = 1; // file des */
+  /* register char*  arg2        asm("rsi") = "Touch Stack"; */
+  /* register int    arg3        asm("rdx") = 11; */
+  /* asm("syscall"); */
+
+  // Num times to push
+  int count = 1<<16;
+
+
+  int i;
+  // Touch a bunch of stack pages
+  for(i=0; i < count; i++){
+    asm("pushq $42");
+  }
+
+  // Pop them all off
+  for(i=0; i < count; i++){
+    asm("popq %rax");
+  }
+
+  printf("Push %d times, %d bytes, %d pages\n", count, count*8, (count*8) / (1<<12) );
+}
 
 void sym_elevate(){
+  touch_stack();
+
   register int    syscall_no  asm("rax") = 1; // write
   register int    arg1        asm("rdi") = 1; // file des
   register char*  arg2        asm("rsi") = "elevate";
