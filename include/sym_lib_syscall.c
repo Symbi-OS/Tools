fb5ab03207b12162fbc1ad283bf8d00498d7f245
Tommy Unger <tunger10@gmail.com>
2021-11-30 18:13:42 -0500

    Add direct asm syscall versions of elevate and lower

--- include/sym_lib_syscall.c
+++ include/sym_lib_syscall.c
@@ -1,4 +1,7 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 
 #include <sys/syscall.h>
@@ -13,29 +16,64 @@ void sym_touch_stack(){
   int i;
   // Touch a bunch of stack pages
   for(i=0; i < count; i++){
-    asm("pushq $42");
+    __asm__("pushq $42":::"memory");
   }
 
   // Pop them all off
   for(i=0; i < count; i++){
-    asm("popq %rax");
+    __asm__("popq %%rax": /*no*/: /*no*/ : "rax");
   }
 }
 
 long sym_check_elevate(){
-    return syscall(448, SYSCALL_CHECK_ELEVATE_STATUS);
+    return syscall(NR_ELEVATE_SYSCALL, SYSCALL_CHECK_ELEVATE_STATUS);
 }
 
 long sym_elevate(){
   // After syscall should be in elevated state
   if(!is_sticky){
-    return syscall(448, SYSCALL_ELEVATE);
+    return syscall(NR_ELEVATE_SYSCALL, SYSCALL_ELEVATE);
   }
+  return -1;
 }
 
 // After syscall should be in lowered state
 long sym_lower(){
   if(!is_sticky){
-    return syscall(448, SYSCALL_LOWER);
+    return syscall(NR_ELEVATE_SYSCALL, SYSCALL_LOWER);
   }
+  return -1;
+}
+
+void sym_elevate_syscall(){
+  register uint64_t    syscall_no  __asm__("rax") = NR_ELEVATE_SYSCALL; // write
+  register uint64_t    arg1        __asm__("rdi") = SYSCALL_ELEVATE; //
+  __asm__ __volatile__ (
+                        "syscall"
+                        : "+r" (syscall_no)
+                        : "r" (arg1), "r" (syscall_no)
+                        : "rcx", "r11", "memory"
+                        );
+}
+
+void sym_lower_syscall(){
+  register uint64_t    syscall_no  __asm__("rax") = NR_ELEVATE_SYSCALL; // write
+  register uint64_t    arg1        __asm__("rdi") = SYSCALL_LOWER; //
+  __asm__ __volatile__ (
+                        "syscall"
+                        : "+r" (syscall_no)
+                        : "r" (arg1), "r" (syscall_no)
+                        : "rcx", "r11", "memory"
+                        );
+}
+
+void sym_check_syscall(){
+  register uint64_t    syscall_no  __asm__("rax") = NR_ELEVATE_SYSCALL; // write
+  register uint64_t    arg1        __asm__("rdi") = SYSCALL_CHECK_ELEVATE_STATUS; //
+  __asm__ __volatile__ (
+                        "syscall"
+                        : "+r" (syscall_no)
+                        : "r" (arg1), "r" (syscall_no)
+                        : "rcx", "r11", "memory"
+                        );
 }
--- include/sym_lib_syscall.h
+++ include/sym_lib_syscall.h
@@ -1,6 +1,7 @@
 #ifndef __SYM_LIB_SYSCALL__
 #define __SYM_LIB_SYSCALL__
 
+#define NR_ELEVATE_SYSCALL 448
 #define SYSCALL_ELEVATE 1
 #define SYSCALL_LOWER -1
 #define SYSCALL_CHECK_ELEVATE_STATUS 0
@@ -10,8 +11,14 @@
 #define STICKY 1
 #define NOT_STICKY 0
 
-// Execute syscall setting elevated bit
+// Execute syscall setting elevated bit. Calls glibc syscall fn.
 extern long sym_elevate();
+
+// Directly executes syscall with assembly.
+extern void sym_elevate_syscall();
+extern void sym_lower_syscall();
+extern void sym_check_syscall();
+
 // Execute syscall clearing elevated bit
 extern long sym_lower();
 
@@ -23,8 +30,5 @@ extern long sym_check_elevate();
 
 // Lock current elevation status until unset
 extern int set_sticky(int is_sticky);
-
-// Restore system IDT
-void sym_restore_system_idt();
 
 #endif
