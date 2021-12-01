6464c0b8f316ac13e5861e698bcecd3067f4693a
Tommy Unger <tunger10@gmail.com>
2021-11-30 20:16:02 -0500

    Simple write shortcut works.

--- examples/write/write_syscall_static.c
+++ examples/write/write_syscall_static.c
@@ -1,24 +1,30 @@
 #include <stdint.h>
 #include <stdio.h>
-#include "elevate.h"
+#include "../../include/sym_lib_syscall.h"
+
+extern char __executable_start;
+extern char __etext;
+
+void touch_every_page_text(){
+  unsigned char *p;
+  p = (unsigned char *) &__executable_start;
+
+  unsigned char c;
+  for(; p < (unsigned char *)&__etext; p+= (1<<12))
+    c = *p;
+}
 
 int main(){
-  int db = 1;
-  while(db);
-
+  touch_every_page_text();
+  sym_touch_stack();
 
   printf("Can we read cr3?\n");
 
+  sym_elevate();
   uint64_t cr3_reg;
-  asm("movq %%cr3,%0"
-      : "=r"(cr3_reg)
-      );
+  asm("movq %%cr3,%0" : "=r"(cr3_reg));
   printf("Cr3 holds %p\n", cr3_reg);
 
-  /* void (*my_entry_SYSCALL_64)() = */
-  /*   ( void(*)() ) 0xffffffff81c00010; */
-
-  /* Can even call a syscall from ring 0 */
   register int    syscall_no  asm("rax") = 1; // write
   register int    arg1        asm("rdi") = 1; // file des
   register char*  arg2        asm("rsi") = "Tommy\n";
@@ -27,14 +33,14 @@ int main(){
 #if 0
   asm("syscall");
 #else
+
   /* asm("jmp 0xffffffff81c00010"); */
   /* my_entry_SYSCALL_64(); */
   int (*my_ksys_write)(unsigned int fd, const char *buf, size_t count) =
-    ( int(*)(unsigned int fd, const char *buf, size_t count) ) 0xffffffff8133e990;
+    ( int(*)(unsigned int fd, const char *buf, size_t count) ) 0xffffffff81206ff0;
   my_ksys_write(1, "Tommy\n", 6);
 #endif
-
-
-
+  sym_lower();
+  printf("Done\n");
   return 0;
 }
