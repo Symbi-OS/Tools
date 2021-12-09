84e310f9bd5f65c0298e69d58190275ac935115f
Tommy Unger <tunger10@gmail.com>
2021-12-09 14:09:12 -0500

    Add some notes to hacks lib

--- include/headers/sym_lib_hacks.h
+++ include/headers/sym_lib_hacks.h
@@ -1,8 +1,15 @@
 #ifndef __SYM_LIB_HACKS__
 #define __SYM_LIB_HACKS__
+// License C 2021-
+// Author: Thomas Unger
+// Level: 0
 
+// Hacks to run when lowered to prep for elevated execution.
+
+// Touch all text pages in executable to prefault.
 void sym_touch_every_page_text();
 
-// Touch some stack pages
+// Touch some stack pages to prefault
 extern void sym_touch_stack();
+
 #endif
