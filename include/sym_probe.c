e53bc2f576ae1296d4b8dd7db4b0b09469e6281e
Tommy Unger <tunger10@gmail.com>
2021-12-09 23:48:55 -0500

    Add code to jump to C in #BP.

--- include/sym_probe.c
+++ include/sym_probe.c
@@ -7,6 +7,58 @@
 // This is the old handler we jmp to after our interposer.
 uint64_t orig_asm_exc_int3;
 
+uint64_t cr3_reg;
+
+void (*myprintk)(char *) = (void *)0xffffffff81bd6f95;
+void (*myprintki)(char *, int) = (void *)0xffffffff81bd6f95;
+
+void tu_c_entry(){
+  char *p = "hey\n";
+  myprintk(p);
+
+  uint64_t my_cr3;
+  asm("movq %%cr3,%0" : "=r"(my_cr3));
+
+  myprintki("got %x\n", my_cr3);
+
+  if(cr3_reg == my_cr3){
+    char *p = "wow, cr3 matches\n";
+    myprintk(p);
+  }else{
+    char *p = "Bummer, no match\n";
+    myprintk(p);
+  }
+}
+
+// HACK: why?
+uint64_t my_entry = (uint64_t) &tu_c_entry;
+
+extern uint64_t int3_jmp_to_c;
+asm(" \
+ .text                          \n\t\
+ .align 16                      \n\t\
+ .global int3_jmp_to_c          \n\t\
+ int3_jmp_to_c:                 \n\t\
+ movb $0x55, 0xffffffff8107dbf0 \n\t\
+ decq (%rsp)                    \n\t\
+ pushq %rbx \n\t\
+ pushq %r12 \n\t\
+ pushq %r13 \n\t\
+ pushq %r14 \n\t\
+ pushq %r15 \n\t\
+ pushq %rbp \n\t\
+ call *my_entry               \n\t\
+ popq %rbp \n\t\
+ popq %r15 \n\t\
+ popq %r14 \n\t\
+ popq %r13 \n\t\
+ popq %r12 \n\t\
+ popq %rbx \n\t\
+ iretq                             \
+");
+
+/* jmp .                          \n\t\ */
+
 // This is the name of our assembly we're adding to the text section.
 // It will be defined at link time, but use this to allow compile time
 // inclusion in C code.
@@ -24,7 +76,7 @@ asm(" \
  iretq                             \
 ");
 
-void sym_interpose_on_int3_ft(char * my_idt){
+void sym_interpose_on_int3_ft_asm(char * my_idt){
   /* sym_print_idt_desc(my_idt, X86_TRAP_BP); */
 
   // Get ptr to pf desc
@@ -41,6 +93,30 @@ void sym_interpose_on_int3_ft(char * my_idt){
   // New handler
   union idt_addr new_asm_exc_addr;
   new_asm_exc_addr.raw = (uint64_t) &bs_asm_exc_int3;
+
+  // Set IDT to point to our new interposer
+  sym_load_desc_from_addr(desc_old, &new_asm_exc_addr);
+
+  /* sym_print_idt_desc(my_idt,  X86_TRAP_BP); */
+}
+
+void sym_interpose_on_int3_ft_c(char * my_idt){
+  /* sym_print_idt_desc(my_idt, X86_TRAP_BP); */
+
+  // Get ptr to pf desc
+  union idt_desc *desc_old;
+  desc_old = sym_get_idt_desc(my_idt, X86_TRAP_BP);
+
+  // save old asm_exc_pf ptr
+  union idt_addr old_asm_exc_int3;
+  sym_load_addr_from_desc(desc_old, &old_asm_exc_int3);
+
+  // swing addr to  bs_asm...
+  orig_asm_exc_int3 = old_asm_exc_int3.raw;
+
+  // New handler
+  union idt_addr new_asm_exc_addr;
+  new_asm_exc_addr.raw = (uint64_t) &int3_jmp_to_c;
 
   // Set IDT to point to our new interposer
   sym_load_desc_from_addr(desc_old, &new_asm_exc_addr);
