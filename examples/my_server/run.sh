3a754b2e50c2590527a24765d390f31f580707ca
Tommy Unger <tunger10@gmail.com>
2021-12-11 15:17:59 -0500

    Accidentally added convenience scripts

--- examples/my_server/run.sh
+++
@@ -1,4 +0,0 @@
-make clean
-make
-make cp_to_disk
-make run_debug_virt
--- examples/text_fault/run.sh
+++
@@ -1,7 +0,0 @@
-make -C ../../include/ clean
-make -C ../../include/
-make clean
-make
-make cp_to_disk
-make run_debug_virt
-
