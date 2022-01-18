c85f7d04c5974aab8ceac07c848663731b6842a8
Tommy Unger <tunger10@gmail.com>
2022-01-18 18:23:19 -0500

    Factor out arch dependent part of sym_lib

--- libs/symlib/arch/x86_64/L0/sym_lib.h
+++ libs/symlib/arch/x86_64/L0/sym_lib.h
@@ -2,10 +2,9 @@
 #define __ARCH_X86_64_SYM_LIB__
 
 #include "L0/sym_lib.h"
-
-long sym_elevate(){
+static long sym_do_syscall(int work){
   register uint64_t    syscall_no  __asm__("rax") = NR_ELEVATE_SYSCALL;
-  register uint64_t    arg1        __asm__("rdi") = SYSCALL_ELEVATE;
+  register uint64_t    arg1        __asm__("rdi") = work;
   if(!is_sticky){
     __asm__ __volatile__ (
                           "syscall"
@@ -14,35 +13,9 @@ long sym_elevate(){
                           : "rcx", "r11", "memory"
                           );
   }
-    return -1;
-}
-
-
-
-long sym_lower(){
-register uint64_t    syscall_no  __asm__("rax") = NR_ELEVATE_SYSCALL;
-  register uint64_t    arg1        __asm__("rdi") = SYSCALL_LOWER;
-  if(!is_sticky){
-    __asm__ __volatile__ (
-                          "syscall"
-                          : "+r" (syscall_no)
-                          : "r" (arg1), "r" (syscall_no)
-                          : "rcx", "r11", "memory"
-                          );
-  }
-  return -1;
-}
-
-long sym_check_elevate(){
-register uint64_t    syscall_no  __asm__("rax") = NR_ELEVATE_SYSCALL;
-register uint64_t    arg1        __asm__("rdi") = SYSCALL_CHECK_ELEVATE_STATUS;
-__asm__ __volatile__ (
-                      "syscall"
-                      : "+r" (syscall_no)
-                      : "r" (arg1), "r" (syscall_no)
-                      : "rcx", "r11", "memory"
-                      );
-return syscall_no;
+  // HACK
+  // Return rax
+  return syscall_no;
 }
 
 #endif
