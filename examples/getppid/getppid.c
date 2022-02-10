598195b2a3a023bdfa0b3277dd91fb71475047c1
Tommy Unger <tunger10@gmail.com>
2022-02-10 17:47:01 -0500

    Update getppid example

--- examples/getppid/getppid.c
+++ examples/getppid/getppid.c
@@ -2,7 +2,9 @@
 #include <unistd.h>
 #include <time.h>
 
-#include "../include/sym_lib.h"
+#include "LINF/sym_all.h"
+
+#define ITERATIONS 1<<24
 
 int bench_time(){
   clock_t start, end;
@@ -10,11 +12,11 @@ int bench_time(){
   start = clock();
   /* Do the work. */
   int i;
-  for(i=0; i< 1<<21; i++)
+  for(i=0; i< ITERATIONS; i++)
     getppid();
 
   end = clock();
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; 
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
   printf("Time used: %f\n", cpu_time_used);
 
   return 0;
@@ -24,16 +26,16 @@ int bench_time_internal(){
   clock_t start, end;
   double cpu_time_used;
 
-  /* int (*getppid_elevated)() = ( int(*)() ) 0xffffffff8107f460; */
-  int (*getppid_elevated)() = ( int(*)() ) 0xffffffff810f62b0;
+  int (*getppid_elevated)() = ( int(*)() ) 0xffffffff810fc0c0;
+
   start = clock();
   /* Do the work. */
   int i;
-  for(i=0; i< 1<<21; i++)
+  for(i=0; i< ITERATIONS; i++)
     getppid_elevated();
 
   end = clock();
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; 
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
   printf("Time used: %f\n", cpu_time_used);
 
   return 0;
@@ -42,25 +44,13 @@ int bench_time_internal(){
 int main(){
   printf("starting main\n");
 
-  // Need generic way to get this.
-  /* tommyu@don:~/Symbi-OS/linux$ nm vmlinux  | grep sys_getppid */
-  /*   ffffffff8107f460 t __do_sys_getppid */
-  /*   ffffffff826ad980 t _eil_addr___ia32_sys_getppid */
-  /*   ffffffff826ad990 t _eil_addr___x64_sys_getppid */
-  /*   ffffffff8107f460 T __ia32_sys_getppid */
-  /*   ffffffff8107f460 T __x64_sys_getppid */
   int (*getppid_elevated)() = ( int(*)() ) 0xffffffff810f62b0;
 
+  bench_time();
+
   sym_elevate();
-
-  /* int ppid=getppid_elevated(); */
-
-  /* bench_time(); */
   bench_time_internal();
+  sym_lower();
 
-  /* sym_lower(); */
-
-  /* printf("elevated ppid is %d \n", ppid); */
-  printf("ppid is %d \n", getppid());
   return 0;
 }
