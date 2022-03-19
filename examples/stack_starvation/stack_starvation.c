56733376a1efd60e5459a53be024a09d513490dd
Tommy Unger <tunger10@gmail.com>
2022-03-18 20:58:29 -0400

    Add DF mitigation copying IDT to new page and swinging IST bit

--- examples/stack_starvation/stack_starvation.c
+++ examples/stack_starvation/stack_starvation.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include <assert.h>
 #include <stdint.h>
@@ -9,8 +10,31 @@
 
 #include "LINF/sym_all.h"
 
+// when we add support for kernel source get this
+// from arch/x86/include/asm/pgtable_types.h
+enum pg_level {
+  PG_LEVEL_NONE,
+  PG_LEVEL_4K,
+  PG_LEVEL_2M,
+  PG_LEVEL_1G,
+  PG_LEVEL_512G,
+  PG_LEVEL_NUM
+};
+
+typedef void (*void_fn_ptr)(unsigned long);
+void_fn_ptr get_fn_address(char *symbol){
+  struct kallsymlib_info *info;
+
+  if (!kallsymlib_lookup(symbol, &info)) {
+    fprintf(stderr, "%s : not found\n", symbol);
+  }
+  return (void_fn_ptr) info->addr;
+}
+
 // Our version of the idt. Not sure about alignment.
 unsigned char my_idt [1<<12] __attribute__ ((aligned (1<<12) ));
+
+unsigned char *kern_pg_for_idt;
 
 // Store system idtr here for later restoration.
 struct dtr system_idtr;
@@ -20,13 +44,32 @@ void show_process_stk_ft_works(){
   sym_touch_stack();
 }
 
+void push_a_lot(){
+  int count = 1 << 19;
+
+  for(int i=0; i< count; i++){
+    __asm__("pushq $42":::"memory");
+  }
+  for(int i=0; i< count; i++){
+    __asm__("popq %%rax": /*no*/: /*no*/ : "rax");
+  }
+
+}
+
 void show_naive_elevation_DFs(){
-  printf("about to elevate and touch stack\n");
+  /* sym_touch_every_page_text(); */
 
+  /* printf("about to elevate and touch stack\n"); */
+
+  /* uint64_t my_cr3_reg; */
+  /* sym_elevate(); */
+
+  /* asm("movq %%cr3,%0" : "=r"(my_cr3_reg)); */
+  /* printf("%lx\n", my_cr3_reg); */
+
+  /* sym_touch_stack(); */
   sym_elevate();
-
-  sym_touch_stack();
-
+  push_a_lot();
   sym_lower();
 }
 
@@ -65,6 +108,131 @@ void show_using_ist_solves_DF(){
   sym_lower();
 }
 
+static inline void
+setPageWriteable(void *desc)
+{
+  struct pte *pte = desc;
+  sym_set_pte_writeable(pte);
+}
+
+static inline bool
+isPageWriteable(void *desc)
+{
+  struct pte * pte = desc;
+  return sym_is_pte_writeable(*pte);
+}
+
+bool updateWriteIfNeeded(void *desc)
+{
+  printf("Is page writable?\n");
+  if (isPageWriteable(desc)) return false;
+
+  printf("If we're here, page shouldnt be writable yet\n");
+  setPageWriteable(desc);
+  printf("Now it should be writable\n");
+  return true;
+}
+
+uint64_t
+getVPNMask(unsigned int level)
+{
+  uint64_t mask = 0; 
+  switch (level) {
+  case PG_LEVEL_4K:
+    mask = ~(((1ULL<<12)-1));
+    break;
+  case PG_LEVEL_2M:
+    mask = ~(((1ULL<<20)-1));
+    break;
+  case PG_LEVEL_1G:
+    mask =  ~(((1ULL<<29)-1));
+    break;
+  case PG_LEVEL_512G:
+    mask =  ~(((1ULL<<38)-1));
+    break;
+  default:
+    assert(0);
+  }
+  fprintf(stderr, "%s(%d) : %lx\n", __func__, level, mask);
+  return mask;
+}
+
+void *
+getPageDesc(uint64_t addr, unsigned int *level)
+{
+  //  returns a copy of the page descriptor
+  sym_elevate();
+  void *rc = sym_get_pte(addr, level);
+  sym_lower();
+  fprintf(stderr, "%s(%lx) : %p pglvl: %d\n", __func__, addr, rc, *level);
+  return  rc;
+}
+
+typedef void * (*vzalloc_t)(unsigned long);
+typedef void (*kvfree_t)(void *);
+
+void * get_aligned_kern_pg(){
+  vzalloc_t vzalloc = get_fn_address("vzalloc");
+  /* printf("About to alloc kern page\n"); */
+
+  sym_elevate();
+  void * p = vzalloc(1 << 12);
+  // Mem leak need this to be page aligned 
+  sym_lower();
+
+  /* printf("got kern page ptr %p\n", p); */
+  assert( ((long unsigned )p % (1<<12) ) == 0);
+  return p;
+}
+
+void free_kern_mem(void *p){
+  // Free page vzalloc'd
+  kvfree_t kvfree  = (kvfree_t) get_fn_address("kvfree");
+  sym_elevate(); kvfree(p); sym_lower();
+}
+
+void show_using_system_ist_solves_DF(){
+  // This actually worked.
+
+  // Let's not think about text faults
+  /* printf("show_using_system_ist_solves_DF()\n"); */
+  sym_touch_every_page_text();
+
+  /* printf("Allocate kern page\n"); */
+  void *p = get_aligned_kern_pg();
+
+  /* printf("Check system descriptor for page fault\n"); */
+  /* sym_print_idt_desc((void *)system_idtr.base, PG_FT_IDX); */
+
+  sym_copy_system_idt(p);
+
+  /* printf("Sanity check copied descriptor matches\n"); */
+  /* sym_print_idt_desc(p, PG_FT_IDX); */
+
+  // modify to use ist
+  /* printf("Sanity check copied descriptor matches\n"); */
+  sym_set_idtr((unsigned long) p, IDT_SZ_BYTES - 1);
+
+  /* printf("Switch onto copied kern page\n"); */
+  sym_make_pg_ft_use_ist((unsigned char *) p);
+
+  /* printf("Verify ist bit is set\n"); */
+  /* sym_print_idt_desc(p, PG_FT_IDX); */
+
+  /* printf("Try to force a starvation\n"); */
+  sym_elevate(); push_a_lot(); sym_lower();
+
+  // Swing idtr back
+  /* printf("Switch back onto system idtr\n"); */
+  sym_load_idtr(&system_idtr);
+
+  /* printf("Check system descriptor for page fault\n"); */
+  /* sym_print_idt_desc((void *) system_idtr.base, PG_FT_IDX); */
+
+  /* printf("Free kern page\n"); */
+  free_kern_mem(p);
+}
+
 void interpose_on_df(){
   // We want to check if another interposition has already taken over idt
   struct dtr check_idtr;
@@ -82,8 +250,51 @@ void interpose_on_df(){
   sym_set_idtr((unsigned long)my_idt, IDT_SZ_BYTES - 1);
 }
 
+void system_interpose_on_df(){
+  // We want to check if another interposition has already taken over idt
+  struct dtr check_idtr;
+  sym_store_idt_desc(&check_idtr);
+
+  printf("kern_pg_for_idt is %p\n", kern_pg_for_idt);
+  if(check_idtr.base != (uint64_t) kern_pg_for_idt){
+    printf("copying the idt for DOUBLE F system\n");
+    // Copy the system idt to userspace if we haven't already.
+    sym_copy_system_idt(kern_pg_for_idt);
+  }
+  printf("About to interpose\n");
+  sym_elevate();
+  sym_interpose_on_df_c(kern_pg_for_idt);
+  sym_lower();
+  printf("About to swing idtr\n");
+
+  // Make our user IDT live!
+  sym_set_idtr((unsigned long)kern_pg_for_idt, IDT_SZ_BYTES - 1);
+}
+
 extern uint64_t cr3_reg;
 void interpose_on_pg_ft(){
+
+  sym_elevate();
+  asm("movq %%cr3,%0" : "=r"(cr3_reg));
+  sym_lower();
+
+  // We want to check if another interposition has already taken over idt
+  struct dtr check_idtr;
+  sym_store_idt_desc(&check_idtr);
+
+  if(check_idtr.base != (uint64_t) &my_idt){
+    printf("copying the idt for PAGE F\n");
+    // Copy the system idt to userspace if we haven't already.
+    sym_copy_system_idt(my_idt);
+  }
+
+  sym_interpose_on_pg_ft_c(my_idt);
+
+  // Make our user IDT live!
+  sym_set_idtr((unsigned long)my_idt, IDT_SZ_BYTES - 1);
+}
+
+void system_interpose_on_pg_ft(){
 
   sym_elevate();
   asm("movq %%cr3,%0" : "=r"(cr3_reg));
@@ -116,15 +327,67 @@ void show_using_idt_interpose_solves_DF(){
   sym_lower();
 }
 
+void show_using_system_idt_interpose_solves_DF(){
+  // This has only been poorly tested.
+
+  printf("About to touch text\n");
+  sym_touch_every_page_text();
+
+  /* system_interpose_on_pg_ft(); */
+
+  // Init kallsym lib
+  /* sym_lib_init(); */
+
+
+  kern_pg_for_idt = get_aligned_kern_pg();
+
+  // XXX REINABLE
+#if 1
+  system_interpose_on_df();
+#endif
+
+  // Test that DF doesn't occur
+  sym_elevate(); push_a_lot(); sym_lower();
+
+  free_kern_mem(kern_pg_for_idt);
+  // Get back on system idtr
+  sym_load_idtr(&system_idtr);
+
+}
+
+void check_idt_and_pf(){
+  sym_touch_every_page_text();
+  struct dtr check_idtr;
+  sym_elevate();
+  sym_store_idt_desc(&check_idtr);
+  sym_lower();
+
+  printf("idtr is %lx\n", check_idtr.base);
+
+  sym_elevate();
+  sym_print_idt_desc((void *)check_idtr.base, DF_IDX); //PG_FT_IDX);
+  sym_lower();
+
+}
+
 /* #define NORMAL_PROCESS 1 */
 /* #define NAIVE_ELEVATION 1 */
 /* #define PREFAULT_ELEVATION 1 */
 /* #define IST_ELEVATION 1 */
-#define IDT_INTERPOSE 1
+#define SYSTEM_IST_ELEVATION 1
+/* #define IDT_INTERPOSE 1 */
+/* #define SYSTEM_IDT_INTERPOSE 1 */
 
 int main(){
   printf("Starting main\n");
   sym_lib_init();
+
+  /* sym_elevate(); */
+  /* push_a_lot(); */
+  /* sym_lower(); */
+
+  /* check_idt_and_pf(); */
+  /* return 0; */
 
 #ifndef NORMAL_PROCESS
   // Store initial system IDTR
@@ -151,15 +414,25 @@ int main(){
   show_using_ist_solves_DF();
 #endif
 
+#ifdef SYSTEM_IST_ELEVATION
+  printf("SYSTEM_IST_ELEVATION\n");
+  show_using_system_ist_solves_DF();
+#endif
+
 #ifdef IDT_INTERPOSE
   printf("IDT_INTERPOSE\n");
   show_using_idt_interpose_solves_DF();
 #endif
 
+#ifdef SYSTEM_IDT_INTERPOSE
+  printf("SYSTEM_IDT_INTERPOSE\n");
+  show_using_system_idt_interpose_solves_DF();
+#endif
+
   printf("Done main\n");
 
-  // Restore system IDTR
 #ifndef NORMAL_PROCESS
+  // Restore system IDTR
   sym_load_idtr(&system_idtr);
 #endif
 }
