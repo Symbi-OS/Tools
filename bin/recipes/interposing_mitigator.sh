caeeb4623ce0aae58d7b037702d1cef7309c165d
Tommy Unger <tunger10@gmail.com>
2022-03-29 15:32:16 -0400

    Major rewrite of interposing mitigator

---
+++ bin/recipes/interposing_mitigator.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+PRINT_RED=1
+function print_color () {
+    echo -n "$(tput setaf $2)$1"
+    # reset
+    echo $(tput setaf 7)
+}
+
+function print_help () {
+    echo script for performing interrupt interposition mitigations
+    echo "flags: -m \"tf\df\" -t <core> -d <debug> "
+    echo
+    echo examples:
+    echo ./copy_idt_interpose_tf.sh -m tf -t 1 -d
+    echo ./copy_idt_interpose_tf.sh -m df -t 1 -d
+}
+function parse_args () {
+    # Call getopt to validate the provided input. 
+    while getopts "dhm:t:" OPTION; do
+        case $OPTION in
+            d)
+                DEBUG=1
+                ;;
+            h)
+                HELP=1
+                print_help
+                exit 0
+                ;;
+            m)
+                MITIGATION=$OPTARG
+                case $MITIGATION in
+                    "tf")
+                        VECTOR=14
+                        ;;
+                    "df")
+                        VECTOR=8
+                        ;;
+                    *)
+                        echo "Incorrect -m option provided:" $MITIGATION
+                        echo bad mitigation: $MITIGATION
+                        ;;
+                esac
+                ;;
+            t)
+                TASKSET_CORE=$OPTARG
+                ;;
+            # v)
+            #     VECTOR=$OPTARG
+            #     ;;
+            *)
+                echo "Incorrect options provided"
+                exit 1
+                ;;
+        esac
+    done
+
+    if [ ! -z "$DEBUG" ]; then
+        echo Apply $MITIGATION mitigation on core $TASKSET_CORE
+    fi
+
+    # Need all.
+    if [ -z "$MITIGATION" ] || [ -z "$TASKSET_CORE" ]; then
+        echo dont have all necessary args
+        echo expect : -m "{\"df\"|\"tf\"}" -t "[ 0, (nproc-1)]"
+        exit -1
+    fi
+}
+
+function get_cur_idt () {
+    # Get initial idt location
+    GET_CUR_IDT="$TASKSET $IDT_TOOL -g"
+
+    if [ ! -z "$DEBUG" ]; then
+        echo
+        echo about to get initial idt:
+        print_color "$GET_CUR_IDT" $PRINT_RED
+    fi
+
+    INITIAL_IDT=$($GET_CUR_IDT)
+
+    if [ ! -z "$DEBUG" ]; then
+        echo initial idt: $INITIAL_IDT
+    fi
+}
+
+function cp_current_idt () {
+    GET_NEW_IDT="$TASKSET $IDT_TOOL -c"
+
+    if [ ! -z "$DEBUG" ]; then
+        echo
+        echo allocate kern page
+        echo copy page $INITIAL_IDT
+        print_color "$GET_NEW_IDT" $PRINT_RED
+    fi
+
+    NEW_IDT=$($GET_NEW_IDT)
+    # Allocate and copy idt onto kern page
+
+    if [ ! -z "$DEBUG" ]; then
+        echo onto page $NEW_IDT
+    fi
+}
+
+function cp_hdl () {
+    # TODO: finish this.
+    # Allocate and copy interposing handler onto kern page
+    GET_HDL_PG="$IDT_TOOL -z $MITIGATION"
+
+    if [ ! -z "$DEBUG" ]; then
+        echo
+        echo allocate kern page
+        echo copy $MITIGATION handler
+        print_color "$GET_HDL_PG" $PRINT_RED
+    fi
+    HDL_PG=$($GET_HDL_PG)
+
+    if [ ! -z "$DEBUG" ]; then
+        echo onto page $HDL_PG
+    fi
+}
+
+function install_hdl () {
+    DO_INSTALL_HDL="$IDT_TOOL -a $NEW_IDT -m addr:$HDL_PG -v $VECTOR"
+    # Stich interposing handler into IDT copy.
+    if [ ! -z "$DEBUG" ]; then
+        echo
+        echo point new idt $NEW_IDT vector $VECTOR at $HDL_PG:
+        print_color "$DO_INSTALL_HDL" $PRINT_RED
+    fi
+    eval $DO_INSTALL_HDL
+
+    if [ ! -z "$DEBUG" ]; then
+        echo hdl installed
+    fi
+}
+
+function install_idt () {
+    INSTALL_IDT="$TASKSET $IDT_TOOL -a $NEW_IDT -i"
+
+    if [ ! -z "$DEBUG" ]; then
+        echo
+        echo make new idt live
+        echo Load IDTR with $NEW_IDT:
+        print_color "$INSTALL_IDT" $PRINT_RED
+    fi
+    OLD_IDT=$($INSTALL_IDT)
+    if [ ! -z "$DEBUG" ]; then
+        echo new idt should be live
+        echo old is $OLD_IDT
+    fi
+
+
+}
+
+# START HERE
+
+IDT_TOOL=$(pwd)/../idt_tool
+
+parse_args "$@"
+
+TASKSET="taskset -c $TASKSET_CORE"
+
+get_cur_idt
+
+cp_current_idt
+
+cp_hdl
+
+install_hdl
+
+install_idt
+
+
+exit 0
+
+# User cmdline control
+while true
+do
+    echo -n type any char from the table above:
+    read input
+    case $input in
+        g)
+            echo QUERY CURRENT IDT
+            $TASKSET $IDT_TOOL -g
+            ;;
+        z)
+            echo QUERY NEW IDT
+            $TASKSET $IDT_TOOL -g
+            ;;
+        v)
+            echo 
+            $TASKSET $IDT_TOOL -a $NEW_IDT -i
+            ;;
+        y)
+            echo INSTALL COPIED IDT
+            $TASKSET $IDT_TOOL -a $NEW_IDT -i
+            ;;
+        x)
+            echo exit case
+            exit
+            ;;
+        *)
+            echo unexpected input $input, exiting
+            exit
+            ;;
+    esac
+done
+
+echo out of loop
+exit
