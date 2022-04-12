8bf00663435bbef0231824318664dac6ec186995
Tommy Unger <tunger10@gmail.com>
2022-04-11 20:25:34 -0400

    Add clock example

--- bin/recipes/mitigate_all.sh
+++ bin/recipes/mitigate_all.sh
@@ -4,6 +4,6 @@ NUM_CPUS=$(nproc)
 
 for i in $(seq 0 $NUM_CPUS)
 do
-	  ./interposing_mitigator.sh -m df -t $i -d
-	  ./interposing_mitigator.sh -m tf -t $i -d
+	  ./interposing_mitigator.sh -m df -t $i
+	  ./interposing_mitigator.sh -m tf -t $i
 done
---
+++ examples/clock/clock.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <time.h>
+
+#include "LINF/sym_all.h"
+
+/* #define ONE_TIMER */
+/* #define SYM_ELEVATE */
+
+
+FILE *fp;
+
+void calc_diff(struct timespec *diff, struct timespec *bigger, struct timespec *smaller)
+{
+	if (smaller->tv_nsec > bigger->tv_nsec)
+    {
+      diff->tv_nsec = 1000000000 + bigger->tv_nsec - smaller->tv_nsec;
+      diff->tv_sec = bigger->tv_sec - 1 - smaller->tv_sec;
+    }
+	else
+    {
+      diff->tv_nsec = bigger->tv_nsec - smaller->tv_nsec;
+      diff->tv_sec = bigger->tv_sec - smaller->tv_sec;
+    }
+}
+
+struct Record
+{
+	size_t size;
+	struct timespec start;
+	struct timespec end;
+};
+
+  void clock_bench(void) {
+
+	struct timespec diff = {0, 0};
+	int l;
+	int loop = 1<<23;
+
+#ifndef ONE_TIMER
+	struct Record *runs;
+
+	runs = mmap(NULL, sizeof(struct Record) * loop, PROT_READ | PROT_WRITE,
+              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	memset(runs, 0, sizeof(struct Record) * loop);
+#endif
+
+#ifdef SYM_ELEVATE
+  sym_elevate();
+#endif
+
+#ifdef ONE_TIMER
+  struct Record stacker;
+#endif
+
+  struct Record outer;
+  clock_gettime(CLOCK_MONOTONIC, &outer.start);
+
+	for (l = 0; l < loop; l++) {
+#ifdef ONE_TIMER
+    clock_gettime(CLOCK_MONOTONIC, &stacker.start);
+    clock_gettime(CLOCK_MONOTONIC, &stacker.end);
+#else
+    clock_gettime(CLOCK_MONOTONIC, &runs[l].start);
+    clock_gettime(CLOCK_MONOTONIC, &runs[l].end);
+#endif
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &outer.end);
+
+#ifdef SYM_ELEVATE
+  sym_lower();
+#endif
+
+
+  calc_diff(&diff, &outer.end, &outer.start);
+  printf("Runtime %ld.%09ld\n", diff.tv_sec, diff.tv_nsec);
+
+	munmap(runs, sizeof(struct Record) * loop);
+
+	return;
+}
+
+int main(void){
+  clock_bench();
+}
