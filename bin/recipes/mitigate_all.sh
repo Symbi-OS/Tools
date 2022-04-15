d93a75229ec3f72b233469afaf81c0c48e72619d
Tommy Unger <tunger10@gmail.com>
2022-04-15 17:07:44 -0400

    Solve core undercounting for mitigator. Now mitigates all cores.

--- bin/recipes/mitigate_all.sh
+++ bin/recipes/mitigate_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-NUM_CPUS=$(nproc)
+# --all is very important if using isolcpu
+NUM_CPUS=$(nproc --all)
 for i in $(seq 0 $(($NUM_CPUS - 1)) )
 do
     # can bug like this
     # (./interposing_mitigator.sh -m df -t $i && \
     #      ./interposing_mitigator.sh -m tf -t $i ) &
+    echo mitigating core $i
     ./interposing_mitigator.sh -m df -t $i
     ./interposing_mitigator.sh -m tf -t $i
 done
