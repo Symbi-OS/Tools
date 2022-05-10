eaa85bd47ce3e413b7e972758171af40c9ad3a3c
Tommy Unger <tunger10@gmail.com>
2022-05-10 17:56:59 -0400

    Add interrupt disable case to elevate lower test

--- examples/elevate_lower/elevate_lower.c
+++ examples/elevate_lower/elevate_lower.c
@@ -3,12 +3,61 @@
 #include <stdlib.h>
 #include "LINF/sym_all.h"
 
+void push_a_lot(){
+  // HACK don't know why seg faults without register usage.
+  register int count = 1 << 13;
+  for(register int i=0; i< count; i++){
+    __asm__("pushq $42":::"memory");
+  }
+  /* printf("pushing a lot \n"); */
+  for(register int i=0; i< count; i++){
+    __asm__("popq %%rax": /*no*/: /*no*/ : "rax");
+  }
+
+}
+#define USE_MODE_SHIFT
+
 int main(int argc, char *argv[]){
+  uint64_t cr3_reg = 0;
   // Assumes some mitigation is used.
+  uint64_t kern_gs;
+  /* uint64_t zero_gs = 0; */
   int count = atoi(argv[1]);
   assert(count >= 1);
+
+  /* push_a_lot(); */
+#ifdef USE_MODE_SHIFT
+  sym_mode_shift(SYM_ELEVATE_FLAG | SYM_INT_DISABLE_FLAG | SYM_INT_DISABLE_FLAG );
+  // NOTE: interrupts disabled
+  /* asm("swapgs"); // get onto kern */
+  /* asm("rdgsbase %0" : "=r"(kern_gs)); */
+  /* asm("swapgs"); // get onto user */
+  /* asm("wrgsbase %0" : : "r"(kern_gs)); */
+  sym_mode_shift(SYM_LOWER_FLAG);
+#else
+  sym_elevate();
+  sym_lower();
+#endif
+
   for(int i = 0; i<count; i++ ){
+#ifdef USE_MODE_SHIFT
+    /* sym_mode_shift(SYM_ELEVATE_FLAG | SYM_INT_DISABLE_FLAG | SYM_DEBUG_FLAG); */
+    sym_mode_shift(SYM_ELEVATE_FLAG );
+#else
     sym_elevate();
+#endif
+    asm("nop");
+    asm("movq %%cr3,%0"
+        : "=r"(cr3_reg)
+        );
+    asm("nop");
+#ifdef USE_MODE_SHIFT
+    /* sym_mode_shift(SYM_LOWER_FLAG | SYM_DEBUG_FLAG); */
+    sym_mode_shift(SYM_LOWER_FLAG);
+#else
     sym_lower();
+#endif
+
   }
+  printf("prove elevation worked by printing cr3 %lx\n", cr3_reg);
 }
