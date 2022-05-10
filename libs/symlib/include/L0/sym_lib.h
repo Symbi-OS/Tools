4576ca605d703cf19a17298db00880061af41ba7
Tommy Unger <tunger10@gmail.com>
2022-05-10 17:50:45 -0400

    Use INT disable to solve gs issue in elevate

--- libs/symlib/include/L0/sym_lib.h
+++ libs/symlib/include/L0/sym_lib.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #ifndef __SYM_LIB_SYSCALL__
 #define __SYM_LIB_SYSCALL__
 
@@ -10,6 +12,34 @@
 #define CHECK_STICKY -1
 #define STICKY 1
 #define NOT_STICKY 0
+
+
+
+/*
+Symbiote flags
+
+Page fault error codes: 
+31              15                             4    3   2   1   0
++-------------------------------------------------+---+---+---+---+
+|                                                 | DB|ID | Q | E |
++-------------------------------------------------+---+---+---+---+
+
+Legend: Set (Clear)
+0 Elevate (lower)
+1 Query (don't)
+2 Iret with Interrupts Disabled (Enabled)
+3 Debug printks for elevate syscall
+*/
+
+#define SYM_LOWER_FLAG 0
+
+#define SYM_ELEVATE_FLAG 1
+#define SYM_QUERY_FLAG (1<<1)
+#define SYM_INT_DISABLE_FLAG (1<<2)
+#define SYM_DEBUG_FLAG (1<<3)
+
+// 'Q'uery, 'D'isable interrupts, 'E'levate
+extern long sym_mode_shift(uint64_t flags);
 
 // Execute syscall setting elevated bit. Calls glibc syscall fn.
 extern long sym_elevate();
--- libs/symlib/src/L0/sym_lib.c
+++ libs/symlib/src/L0/sym_lib.c
@@ -12,16 +12,33 @@ __thread int is_sticky = 0;
 #include "../../arch/x86_64/L0/sym_lib.h"
 #endif
 
+long sym_mode_shift(uint64_t flags){
+  return sym_do_syscall(flags);
+}
 
 long sym_elevate(){
-  return sym_do_syscall(SYSCALL_ELEVATE);
+  // XXX HACK This assumes we can just clobber user's gsbase...
+  // I believe it also assumes no core migration...
+  uint64_t kern_gs;
+
+  long ret = sym_mode_shift( SYM_ELEVATE_FLAG | SYM_INT_DISABLE_FLAG );
+  // XXX make this architecture agnostic
+  asm("swapgs"); // get onto kern gs
+  asm("rdgsbase %0" : "=rm"(kern_gs) :: ); // Store kern gs
+  asm("swapgs"); // get onto user gs
+  asm("wrgsbase %0" :: "r"(kern_gs) ); // Overwrite user gs with kern gs
+  asm("sti"); // make interruptable
+  return ret;
 }
 
 long sym_lower(){
-  return sym_do_syscall(SYSCALL_LOWER);
+  /* return sym_do_syscall(SYSCALL_LOWER); */
+  return sym_mode_shift(SYM_LOWER_FLAG);
+
 }
 
 long sym_check_elevate(){
-  return sym_do_syscall(SYSCALL_CHECK_ELEVATE_STATUS);
+  /* return sym_do_syscall(SYSCALL_CHECK_ELEVATE_STATUS); */
+  return sym_mode_shift(SYM_QUERY_FLAG);
 }
 
