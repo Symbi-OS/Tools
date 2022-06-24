33cf1a09e2ae7cf25c4a92cc4949a930e0342ad1
Ryan Sullivan <ryanbakersullivan@gmail.com>
2022-06-24 16:45:29 -0400

    minor changes

--- bin/snapshot_tool.c
+++ bin/snapshot_tool.c
@@ -1,8 +1,14 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/io.h>
+#include <sys/mman.h>
 
 #include "../libs/symlib/include/LINF/sym_all.h"
+
 #include "snapshot_tool.h"
 
 uint64_t get_fn_address(char *symbol){
@@ -17,40 +23,69 @@ uint64_t get_fn_address(char *symbol){
 int main(int argc, char * argv[]){
   uint64_t cr3, db_reg = 0, core = 0;
   void * pg_tbl = NULL;
-  char * fn_name;
+  char * fn_name;// * target;
   uint64_t fn_addr;
   struct scratchpad * sp; 
 
   if(argc < 3)
     return 1;
 
+
   fn_name = argv[1];
   db_reg = atoi(argv[2]);
   core = atoi(argv[3]);
   
+  // set probe on core
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+  CPU_SET(core, &mask);
+  sched_setaffinity(0, sizeof(mask), &mask);
+
   sp = (struct scratchpad *)get_scratch_pg(core);
   sym_lib_init();
   fn_addr = get_fn_address(fn_name);
   printf("SETTING TRIGGER AT 0x%lx\n", fn_addr);
-  sym_set_db_probe((uint64_t)fn_addr, db_reg, DB_GLOBAL); 
+  sym_set_db_probe(fn_addr, db_reg, DB_GLOBAL); 
+  
+  // launch target
 
-  printf("IDLE UNTIL TRIGGER HIT\n");
-
+  printf("PID: %d\n", getpid());
+  
   sym_elevate();
-  sp->get.dr_hit = 9;
-  //sym_lower();
-
-  while(sp->get.dr_hit > 3);
-
-  //sym_elevate();
   cr3 = sp->get.cr3;
   sym_lower();
 
-  uint64_t phys = (cr3 >> 12) << 12;
-  uint64_t virt = 0xffff888000000000;
-  pg_tbl = (void *)(phys + virt);
+  pg_tbl = (void *)phys_to_virt(cr3);
 
-  printf("CR3: 0x%lx\nPG_TBL ADDR: %p\n", cr3, pg_tbl);
+/* 
+  int fd = open("/dev/mem", O_RDWR);
+  if(fd == -1){
+    printf("%s\n", strerror(errno));
+    return 1;
+  }
+
+  uint64_t phys = (cr3 >> 12) << 12;
+  pg_tbl = mmap(NULL, PG_SZ, PROT_READ, MAP_PRIVATE, fd, phys);
+  if(pg_tbl == MAP_FAILED){
+    printf("%s\n", strerror(errno));
+    return 1;
+  }
+*/
+  printf("CR3: 0x%lx\nPG_TBL ADDR: %p\nPG_SZ: 0x%llx\n", cr3, pg_tbl, PG_SZ);
+
+  char fname[100];
+  strcpy(fname, fn_name);
+  strcat(fname, ".snapshot");
+  int snapshot = open(fname, O_CREAT);
+  if(snapshot < 0)
+    printf("%s\n", strerror(errno));
+  printf("FILE DESC: %d\n", snapshot);
+  char * buf = "TEST";
+
+  ssize_t wr = write(snapshot, buf, 4);
+  printf("WRITE LEN: %ld\n", wr);
+  if(wr < 0)
+    printf("%s\n", strerror(errno));
 
   return 0;
 }
