693fe7c688e1494f0509313488c50a467308cda2
FlareCoding <albert.slepak@gmail.com>
2022-10-23 22:09:03 -0400

    removed the dependency on kallsymlib

--- examples/int3_probe/trivial_int3.c
+++ examples/int3_probe/trivial_int3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "LINF/sym_all.h"
-#include "../../../kallsymlib/kallsymlib.h"
 void check_on_probe(uint64_t addr){
   sym_elevate();
   printf("1st byte getpid is %#x \n", *((unsigned char *) addr));
