566b249973d4729c606c634cf5445357d412a8d5
Tommy Unger <tunger10@gmail.com>
2022-10-25 14:23:36 -0400

    modified sc_lib to utilize the new libSym.so, giving it access to all Symlib functions (allows for dynamic locating of ksys_write in sc_lib), also taskset the testing example as core switching is an even greater issue it seems on baremetal

--- bin/shortcut/sc_lib.c
+++ bin/shortcut/sc_lib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "../../../Symlib/include/L0/sym_lib.h"
+#include "../../Symlib/include/LINF/sym_all.h"
+//#include "../../../Symlib/include/L0/sym_lib.h"
 
 typedef void (*void_fn_ptr)(unsigned long);
 typedef int (*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
@@ -10,8 +11,8 @@ ksys_write_t ksys_wr_sc;
 void __attribute__ ((constructor)) init(void) {
   // function that is called when the library is loaded
   // printf("FUNC_ADDR_RET: %p\n", (void *)kallsyms_get_name("ksys_write")); 
-  ksys_wr_sc = (ksys_write_t)0xffffffff813670f0;
- sym_elevate();
+  ksys_wr_sc = (ksys_write_t)sym_get_fn_address("ksys_write");
+  sym_elevate();
 }
 
 void __attribute__ ((destructor)) cleanUp(void) {
