8058145ba1b5771fc9284daee770a5d70b47f6a8
Tommy Unger <tunger10@gmail.com>
2022-10-28 12:35:31 -0400

    Add env var in constructor for Ryan

--- examples/dynam_interpose/interpose/hijack.c
+++ examples/dynam_interpose/interpose/hijack.c
@@ -1,5 +1,32 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "helper.h"
+
+#define BUFSIZE 80
+
+// Show example reading envt var
+void __attribute__ ((constructor)) setup(void) {
+  char buf[BUFSIZE];
+  char *envvar = "MY_ENV_VAR";
+
+  // Make sure envar actually exists
+  if(!getenv(envvar)){
+    fprintf(stderr, "The environment variable %s was not found.\n", envvar);
+    fprintf(stderr, "So I won't touch the flag\n");
+  } else {
+    if(snprintf(buf, BUFSIZE, "%s", getenv(envvar)) >= BUFSIZE){
+      fprintf(stderr, "BUFSIZE of %d was too small. Aborting\n", BUFSIZE);
+      exit(1);
+    }
+    printf("MY_ENV_VAR: %s\n", buf);
+    fprintf(stderr, "So I will set the flag\n");
+
+  }
+
+
+}
+
+
 void expected(){
   printf("in hijacked lib, expectations upended\n");
   helper();
