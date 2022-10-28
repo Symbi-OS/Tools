a2e252a5b64d3a4897ad68406a35f076535d48f2
Ryan Sullivan <ryanbakersullivan@gmail.com>
2022-10-28 15:24:50 -0400

    finished initial implementation of the shortcut command line tool TODO: expand sc_lib

--- bin/shortcut/sc_lib.c
+++ bin/shortcut/sc_lib.c
@@ -1,18 +1,35 @@
+#include <sys/syscall.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "../../Symlib/include/LINF/sym_all.h"
 //#include "../../../Symlib/include/L0/sym_lib.h"
 
+#define BUFSIZE 80
+
 typedef void (*void_fn_ptr)(unsigned long);
 typedef int (*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
 typedef int (*ksys_read_t)(unsigned int fd, const char *buf, size_t count);
 ksys_write_t ksys_wr_sc;
+ksys_read_t ksys_rd_sc;
+uint32_t opt = 0;
 
 void __attribute__ ((constructor)) init(void) {
   // function that is called when the library is loaded
-  // printf("FUNC_ADDR_RET: %p\n", (void *)kallsyms_get_name("ksys_write")); 
-  ksys_wr_sc = (ksys_write_t)sym_get_fn_address("ksys_write");
-  sym_elevate();
+  char buf[BUFSIZE];
+  char* envvar = "SHORTCUT_OPTIONS";
+  
+  if(getenv(envvar)){
+    if(snprintf(buf, BUFSIZE, "%s", getenv(envvar)) >= BUFSIZE){
+      fprintf(stderr, "BUFSIZE OF %d TOO SMALL. ABORTING\n", BUFSIZE);
+      exit(1);
+    } else {
+      opt = atoi(buf);
+      ksys_wr_sc = (ksys_write_t)sym_get_fn_address("ksys_write");
+      ksys_rd_sc = (ksys_read_t)sym_get_fn_address("ksys_read");
+      sym_elevate();
+    }
+  }
 }
 
 void __attribute__ ((destructor)) cleanUp(void) {
@@ -20,8 +37,18 @@ void __attribute__ ((destructor)) cleanUp(void) {
   sym_lower();
 }
 
-ssize_t write(int fd, const void* buf, size_t len){
+ssize_t read(int fd, const void* buf, size_t len){
   // interpose shortcutted function
-  return ksys_wr_sc(fd, buf, len);
+  if(opt & 1)
+    return ksys_rd_sc(fd, buf, len);
+  else
+    return syscall(SYS_read, fd, buf, len);
 }
 
+ssize_t write(int fd, const void* buf, size_t len){
+  // interpose shortcutted function
+  if(opt & 4)
+    return ksys_wr_sc(fd, buf, len);
+  else
+    return syscall(SYS_write, fd, buf, len);
+}
