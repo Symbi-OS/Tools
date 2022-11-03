e8db60f25ff7ad126910b6442357a0819946d7aa
Tommy Unger <tunger10@gmail.com>
2022-11-03 12:09:02 -0400

    added in testing framework for comparing static vs dynamic linking of example program TODO: gather data to confirm hypothesis of negligible performance difference

---
+++ examples/static_v_dynam/test.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(){
+  int i,fd;
+  char* buf = "\0";
+  fd = open("/dev/null", O_WRONLY);
+
+  for(i = 0; i < 10000000; i++){
+    write(fd,buf,1);
+  }
+  for(i = 0; i < 10000000; i++){
+    read(fd,buf,1);
+  }
+}
