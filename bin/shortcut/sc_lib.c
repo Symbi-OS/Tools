9f88b474853af18b9ab1272fa0f535c22e579185
Tommy Unger <tunger10@gmail.com>
2022-11-07 15:04:11 -0500

    Add script and start modifying shortcut lib.

--- bin/shortcut/sc_lib.c
+++ bin/shortcut/sc_lib.c
@@ -1,35 +1,150 @@
+  #define _GNU_SOURCE
+
 #include <sys/syscall.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "../../Symlib/include/LINF/sym_all.h"
-//#include "../../../Symlib/include/L0/sym_lib.h"
+// Include for bool
+#include <stdbool.h>
 
-#define BUFSIZE 80
+#include <stdio.h>
+#include <dlfcn.h>
 
-typedef void (*void_fn_ptr)(unsigned long);
-typedef int (*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
-typedef int (*ksys_read_t)(unsigned int fd, const char *buf, size_t count);
-ksys_write_t ksys_wr_sc;
-ksys_read_t ksys_rd_sc;
-uint32_t opt = 0;
+// Struct that contains all flags for function interposition
+struct fn_ctrl{
+  bool enter_elevated;
+  bool return_elevated;
+  bool do_shortcut;
+};
+
+// Just learned this black magic
+extern char** environ;
+
+// #define BUFSIZE 4096
+
+// typedef void (*void_fn_ptr)(unsigned long);
+// typedef int (*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
+// typedef int (*ksys_read_t)(unsigned int fd, const char *buf, size_t count);
+// ksys_write_t ksys_wr_sc;
+// ksys_read_t ksys_rd_sc;
+
+
+// Function that prints string in red
+void print_red(const char* str){
+    printf("\033[1;31m%s\033[0m", str);
+}
+
+// This is dumb, but we need something quick.
+// We've used these to elevate
+
+// We've used these to lower
+
+// We've used these to shortcut
+struct fn_ctrl rd_ctrl; //wr_ctrl, getpid, getppid;
+
+
+void print_sc_envt_vars(){
+  // Produce array of environment variables
+  for (size_t i = 0; environ[i] != NULL; i++) {
+    // ELEVATE
+    if (strncmp(environ[i], "ELEVATE", 7) == 0) {
+      // Print the environment variable, skipping the first 8 chars and removing the last 2 chars
+      printf("%s\n", environ[i]);
+      // printf("%s\n", environ[i] + 8);
+      // If the environment variable suffix is 'read=1' set the read fn
+      // if(strncmp(environ[i] + 8, "read=1", 6) == 0){
+      //   printf("Need to elevate %s\n", environ[i] + 8);
+      //  //rd_ctrl.do_shortcut = true;
+
+      // }
+    }
+    // LOWER
+    if (strncmp(environ[i], "LOWER", 5) == 0) {
+      printf("%s\n", environ[i]);
+    }
+    // SHORTCUT
+    if (strncmp(environ[i], "SHORTCUT", 8) == 0) {
+      printf("%s\n", environ[i]);
+    }
+  }
+
+}
+
+// Function that initializes a fn_ctrl struct
+void init_fn_ctrl(struct fn_ctrl* ctrl){
+  ctrl->enter_elevated = false;
+  ctrl->return_elevated = false;
+  ctrl->do_shortcut = false;
+}
+
+// Elevate fn
+void elevate_fn(struct fn_ctrl* ctrl){
+}
+void lower_fn(struct fn_ctrl* ctrl){
+}
+void shortcut_fn(struct fn_ctrl* ctrl){
+}
+
+
+// Use envt vars to process function controls
+void process_fn_ctrls(){
+  printf("process_fn_ctrls\n\n");
+
+  // Elevate and lower NYI
+
+  // Find functions to shortcut.
+  for (size_t i = 0; environ[i] != NULL; i++) {
+    if (strncmp(environ[i], "SHORTCUT", 8) == 0) {
+      // Compare environment variable to known strings
+      char *wr_str = "SHORTCUT_write_TO_ksys_write=1";
+      char *rd_str = "SHORTCUT_read_TO_ksys_read=1";
+      if(strncmp(environ[i], wr_str, strlen(wr_str)) == 0){
+        printf("Need to shortcut write to ksys_write\n");
+      } else if (strncmp(environ[i], rd_str, strlen(rd_str)) == 0) {
+        printf("Need to shortcut rd to ksys_rd\n");
+      } else{
+        // Print error with unexpected shortcut and exit
+        printf("Unexpected shortcut: %s\n", environ[i]);
+        exit(1);
+      }
+      
+    }
+  }
+
+
+}
+
+// Returns true if the environment variable is found
+bool envt_var_exists(char *var_name){
+  for (size_t i = 0; environ[i] != NULL; i++) {
+    if (strncmp(environ[i], var_name, strlen(var_name)) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
 
 void __attribute__ ((constructor)) init(void) {
   // function that is called when the library is loaded
-  char buf[BUFSIZE];
-  char* envvar = "SHORTCUT_OPTIONS";
-  
-  if(getenv(envvar)){
-    if(snprintf(buf, BUFSIZE, "%s", getenv(envvar)) >= BUFSIZE){
-      fprintf(stderr, "BUFSIZE OF %d TOO SMALL. ABORTING\n", BUFSIZE);
-      exit(1);
-    } else {
-      opt = atoi(buf);
-      ksys_wr_sc = (ksys_write_t)sym_get_fn_address("ksys_write");
-      ksys_rd_sc = (ksys_read_t)sym_get_fn_address("ksys_read");
-      sym_elevate();
-    }
+  print_red("Library loaded\n");
+
+  // for debugging
+  print_sc_envt_vars();
+
+  // Initialize function controls
+  process_fn_ctrls();  // Initialize the function control structs
+
+  // If environment variable 'BEGIN_ELE=1' is set, elevate
+  if (envt_var_exists("BEGIN_ELE=1")) {
+    printf("Elevating\n");
+  //  elevate_fn(&rd_ctrl);
   }
+
+
+
+  print_red("Done initializing\n");
+
 }
 
 void __attribute__ ((destructor)) cleanUp(void) {
@@ -37,18 +152,31 @@ void __attribute__ ((destructor)) cleanUp(void) {
   sym_lower();
 }
 
+// Our interposer
+// Function pointer typedef for read glibc call
+typedef ssize_t (*read_t)(int fd, void *buf, size_t count);
+// Function pointer typedef for write glibc call
+typedef ssize_t (*write_t)(int fd, const void *buf, size_t count);
+
+read_t real_read = NULL;
+write_t real_write = NULL;
 ssize_t read(int fd, void* buf, size_t len){
-  // interpose shortcutted function
-  if(opt & 1)
-    return ksys_rd_sc(fd, buf, len);
-  else
-    return syscall(SYS_read, fd, buf, len);
+   if (!real_read) {
+    real_read = dlsym(RTLD_NEXT, "read");
+    printf("real_read is %p \n", real_read);
+  }
+
+  // Call real read
+  return real_read(fd, buf, len);
 }
 
 ssize_t write(int fd, const void* buf, size_t len){
-  // interpose shortcutted function
-  if(opt & 4)
-    return ksys_wr_sc(fd, buf, len);
-  else
-    return syscall(SYS_write, fd, buf, len);
+  // If real_write is null, get it from dlsym
+  if (!real_write) {
+    real_write = dlsym(RTLD_NEXT, "write");
+    printf("real_write is %p \n", real_write);
+  }
+  // Call real write
+  return real_write(fd, buf, len);
+
 }
---
+++ bin/shortcut/shortcut.sh
@@ -0,0 +1,261 @@
+#!/bin/bash
+# set -x
+set -e
+
+
+function print_help () {
+    printf "###################################"
+
+# square brackets [optional option]
+# angle brackets <required argument>
+# curly braces {default values}
+# parenthesis (miscellaneous info)
+
+    printf "\nshortcut.sh [args] -- <app> [app args]\n\n"
+    printf "Script for launching apps with options for elevation and shortcutting.\n\n"
+
+    printf "Flags:\n\n"
+
+    # Passthrough
+
+
+    # kElevate related options
+
+    printf "\t-p: passthrough mode, run application without interposer library.\n"
+    printf "\t\t Ignores all other args, \n"
+
+    printf "The following options may interact with kElevate\n\n"
+
+    printf "\t-be / -bl: begin with executable elevated / lowered\n\n"
+
+    printf "These ones  can be repeated to specify multiple functions:\n"
+    printf "\t-e <fns>: Elevate around these functions\n"
+    printf "\t-l <fns>: Lower around these functions\n"
+    printf "\t-s <fn1->fn2>: Shortcut between these functions: eg: write->ksys_write\n\n"
+
+    printf "Debugging options\n\n"
+    printf "\t-v: verbose mode for debugging\n"
+    printf "\t-d: dry run, do everything EXCEPT running the application\n\n"
+
+    printf "###################################"
+
+}
+
+function parse_args () {
+    # Call getopt to validate the provided input.
+    while getopts "b:e:dhl:pr:s:vw:" OPTION; do
+        case "${OPTION}" in
+            # Begin with executable elevated / lowered
+            b)
+                # echo OPTION: $OPTION
+                # echo process b flag with "$OPTARG"
+                BEGIN_ELE=$OPTARG
+                case $BEGIN_ELE in
+                    "e")
+                        BEGIN_ELE=1
+                        ;;
+                    "l")
+                        # Do nothing, this is the default
+                        ;;
+                    *)
+                        echo "Incorrect -b option provided:" $BEGIN_ELE
+                        echo bad begin: $BEGIN_ELE
+                        ;;
+                esac
+                ;;
+            d) # dry run
+                DRY_RUN=1
+                ;;
+            # VERBOSE
+            v)
+                # echo process d flag
+                VERBOSE=1
+                ;;
+            # elevate around these functions
+            e)
+                # echo process e flag with "$OPTARG"
+                # Add to list of functions to elevate around
+                ELEVATE_FN_LIST="$ELEVATE_FN_LIST $OPTARG"
+                ;;
+            # lower around these functions
+            l)
+                # echo process l flag with "$OPTARG"
+                # Add to list of functions to lower around
+                LOWER_FN_LIST="$LOWER_FN_LIST $OPTARG"
+                ;;
+            # passthrough mode, run without interposer library.
+            p)
+                # echo process p flag
+                PASSTHROUGH=1
+                ;;
+            # shortcut between these functions
+            s)
+                # echo process s flag with "$OPTARG"
+                # Add to list of functions to shortcut between
+                SHORTCUT_FN_LIST="$SHORTCUT_FN_LIST $OPTARG"
+                ;;
+            h)
+                print_help
+                exit 0
+                ;;
+            *)
+                echo "Incorrect option provided:" "$OPTION"
+                echo bad option: "$OPTION"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+# A function that checks the sanity of provided arguments.
+function check_opts () {
+    # This is currently mostly a stub.
+
+    # Check that the user provided an executable to run
+
+    if [ -z "$APP_AND_ARGS" ]; then
+        echo "No executable provided"
+        exit 1
+    fi
+
+    # If verbose, print out the arguments
+    if [ -n "$VERBOSE" ]; then
+        echo "Script arguments: $SCRIPT_ARGS"
+        echo "APP_AND_ARGS: $APP_AND_ARGS"
+        echo "ELEVATE_FN_LIST: $ELEVATE_FN_LIST"
+        echo "LOWER_FN_LIST: $LOWER_FN_LIST"
+        echo "SHORTCUT_FN_LIST: $SHORTCUT_FN_LIST"
+        echo "BEGIN_ELE: $BEGIN_ELE"
+    fi
+
+    return
+}
+
+# Create environment variables that control how the interposer
+# runs the executable.
+create_envt_vars () {
+
+    # If passthrough mode, don't bother, just return
+    if [ -n "$PASSTHROUGH" ]; then
+        # if VERBOSE
+        if [ -n "$VERBOSE" ]; then
+            echo "Passthrough mode, not creating envt vars"
+            return
+        fi
+    fi
+
+    ENVT_VARS=""
+
+    # If we should begin elevated, add that to the environment
+    if [ -n "$BEGIN_ELE" ]; then
+        ENVT_VARS="$ENVT_VARS BEGIN_ELE=$BEGIN_ELE"
+    fi
+
+    # Create a string of functions to elevate around
+    for fn in $ELEVATE_FN_LIST; do
+        ENVT_VARS="$ENVT_VARS ELEVATE_$fn=1"
+    done
+
+    # Create a string of functions to lower around
+    for fn in $LOWER_FN_LIST; do
+        ENVT_VARS="$ENVT_VARS LOWER_$fn=1"
+    done
+
+    # Create a string of functions to shortcut between
+    for fn in $SHORTCUT_FN_LIST; do
+        # TODO: I couldn't figure out how to use a '->' in an envt var
+
+        # Assert there is exactly one '-' on the line
+        if [ $(echo $fn | grep -o '-' | wc -l) -ne 1 ]; then
+            echo "Incorrect shortcut format: $fn"
+            exit 1
+        fi
+        # Assert there is exactly one '>' on the line
+        if [ $(echo $fn | grep -o '>' | wc -l) -ne 1 ]; then
+            echo "Incorrect shortcut format: $fn"
+            exit 1
+        fi
+
+        # Split the string on the ->, prefix and suffix
+        prefix=$(echo $fn | cut -d'-' -f1)
+        suffix=$(echo $fn | cut -d'>' -f2)
+
+        ENVT_VARS="$ENVT_VARS SHORTCUT_${prefix}_TO_${suffix}=1"
+    done
+
+    if [ -n "$VERBOSE" ]; then
+        echo "ENVT_VARS: $ENVT_VARS"
+    fi
+}
+
+# Execute the application with the interposer library and all envt vars.
+function run () {
+    # We will run this program.
+    RUN_CMD=$APP_AND_ARGS
+
+    # If passthrough mode, skip
+
+    # If not passthrough mode, add the interposer library and envt vars to the command
+    if [ -z "$PASSTHROUGH" ]; then
+        LD_LIBRARY_PATH="$LD_LIBRARY_PATH:../../../Symbi-OS/Symlib/dynam_build"
+        LD_LIBRARY_PATH='/home/sym/Symbi-OS/Symlib/dynam_build'
+        LD_LIBRARY_ENVT_VAR="LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+
+        RUN_CMD="$LD_LIBRARY_ENVT_VAR $ENVT_VARS $INTERPOSER_ENVT_VAR $RUN_CMD " 
+    fi
+
+    # If VERBOSEging, print out the command
+    if [ -n "$VERBOSE" ]; then
+        echo "RUN_CMD: $RUN_CMD"
+    fi
+
+    # Run the command
+    # TODO: Someone tell me why this doesn't work
+    # $RUN_CMD
+    # But this does...
+    # If dry run, don't actually run the command
+    if [ -z "$DRY_RUN" ]; then
+        eval "$RUN_CMD"
+    else
+        # if VERBOSE
+        if [ -n "$VERBOSE" ]; then
+            echo "Dry run, not running command"
+        fi
+    fi
+
+}
+
+# Program starts here
+# Todo probably want to keep this as an array, but it's breaking somewhere...
+INPUT_ARGS="$@"
+
+# if [[ "$(declare -p "$INPUT_ARGS")" =~ "declare -a" ]]; then
+#     echo array
+# else
+#     echo no array
+# fi
+# This is provided to separate the application arguments from the shortcut
+SEARCH_STR="--"
+
+# Return the prefix of the array up to the search string, these are for this script
+SCRIPT_ARGS="${INPUT_ARGS%%"$SEARCH_STR"*}"
+
+# Return the suffix of the array after the search string, these are for the application
+APP_AND_ARGS="${INPUT_ARGS##*"$SEARCH_STR"}"
+
+# echo INPUT_ARGS: $INPUT_ARGS
+# echo SCRIPT_ARGS: $SCRIPT_ARGS
+# echo APP_AND_ARGS: $APP_AND_ARGS
+
+parse_args $SCRIPT_ARGS
+
+check_opts
+
+create_envt_vars
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+INTERPOSER_LIB="$SCRIPT_DIR/sc_lib.so"
+INTERPOSER_ENVT_VAR="LD_PRELOAD=$INTERPOSER_LIB"
+
+run
+# todo, add error checking e.g. no element of ELEVATE_FN_LIST is in LOWER_FN_LIST
