dbd1d92a28bac16ef9fe54bb0a5427d8eedddb56
Tommy Unger <tunger10@gmail.com>
2022-11-09 16:25:43 -0500

    Refactor fn ctrl config

--- bin/shortcut/sc_lib.c
+++ bin/shortcut/sc_lib.c
@@ -31,6 +31,7 @@ void print_red(const char* str){
     printf("\033[1;31m%s\033[0m", str);
 }
 
+#if 0
 void print_sc_envt_vars(){
   // Produce array of environment variables
   for (size_t i = 0; environ[i] != NULL; i++) {
@@ -57,6 +58,7 @@ void print_sc_envt_vars(){
   }
 
 }
+#endif
 
 // Function that initializes a fn_ctrl struct
 void init_fn_ctrl(struct fn_ctrl* ctrl){
@@ -64,14 +66,6 @@ void init_fn_ctrl(struct fn_ctrl* ctrl){
   ctrl->enter_elevated = false;
   ctrl->return_elevated = false;
   ctrl->do_shortcut = false;
-}
-
-// Elevate fn
-void elevate_fn(struct fn_ctrl* ctrl){
-}
-void lower_fn(struct fn_ctrl* ctrl){
-}
-void shortcut_fn(struct fn_ctrl* ctrl){
 }
 
 // Returns true if the environment variable is found
@@ -89,18 +83,13 @@ void __attribute__ ((constructor)) init(void) {
   print_red("Library loaded\n");
 
   // for debugging
-  print_sc_envt_vars();
-
-  // Initialize function controls
-  // process_fn_ctrls();  // Initialize the function control structs
+  // print_sc_envt_vars();
 
   // If environment variable 'BEGIN_ELE=1' is set, elevate
   if (envt_var_exists("BEGIN_ELE=1")) {
     printf("Elevating\n");
     sym_elevate();
   }
-
-
 
   print_red("Done initializing\n");
 
@@ -119,12 +108,9 @@ typedef ssize_t (*write_t)(int fd, const void *buf, size_t count);
 read_t real_read = NULL;
 write_t real_write = NULL;
 
-// Are we doing elevating or lowering on this call?
-// Can't do both elevating and lowering
-// If shortcutting, need to either be elevated from the beginning or elevate on the fly
-// If shortcutting, lower can't be used.
-
-struct fn_ctrl wr_ctrl;
+// Function control structs
+struct fn_ctrl write_ctrl;
+// struct fn_ctrl rd_ctrl;
 
 ssize_t read(int fd, void* buf, size_t len){
   if (!real_read) {
@@ -136,27 +122,60 @@ ssize_t read(int fd, void* buf, size_t len){
   // Call real read
   return real_read(fd, buf, len);
 }
-// Takes buffer, prefix, fn, and suffix returns a string.
-  void build_envt_var(char * buf, char * prefix, const char * fn, char * suffix){
-
+// Takes buffer, prefix, fn, and suffix
+void build_envt_var(char * buf, char * prefix, const char * fn, char * suffix){
     // Copy prefix
     strcpy(buf, prefix);
     // Copy fn
     strcat(buf, fn);
     // Copy suffix
     strcat(buf, suffix);
-  printf("elevate_fn: %s\n", buf);
+    printf("elevate_fn: %s\n", buf);
+}
 
+// Only works for ksys
+void build_shortcut_envt_var(char * buf, char * prefix, const char * fn, char * suffix){
 
-  }
+    // Copy prefix "SHORTCUT_"
+    strcpy(buf, prefix);
+    // Copy fn
+    strcat(buf, fn);
+    // Copy _TO_
+    strcat(buf, "_TO_");
+    strcat(buf, "ksys_");
+    strcat(buf, fn);
+    // Copy suffix
+    strcat(buf, suffix);
 
-// Sets elevate and lower flags for function
-void config_fn(struct fn_ctrl *wr_ctrl, const char *fn){
-  init_fn_ctrl(wr_ctrl);
-  // Check for elevate envt variable
-  printf("Checking for elevate envt variable\n");
-  // printf("envt var:%s\n", "ELEVATE" fn "=1");
+    printf("elevate_fn: %s\n", buf);
+}
 
+void config_fn_ctrl_for_elevate(struct fn_ctrl * ctrl){
+  // Build the environment variable
+    ctrl->sandwich_fn = true;
+    ctrl->enter_elevated = true;
+    ctrl->return_elevated = false;
+    ctrl->do_shortcut = false;
+}
+
+void config_fn_ctrl_for_lower(struct fn_ctrl * ctrl){
+  // Build the environment variable
+    ctrl->sandwich_fn = true;
+    ctrl->enter_elevated = false;
+    ctrl->return_elevated = true;
+    ctrl->do_shortcut = false;
+}
+
+// void config_fn_for_shortcut(struct fn_ctrl * ctrl){
+//   // Build the environment variable
+//     ctrl->sandwich_fn = true;
+//     ctrl->enter_elevated = false;
+//     ctrl->return_elevated = false;
+//     ctrl->do_shortcut = true;
+// }
+
+void config_fn_elevate(struct fn_ctrl * ctrl, const char * fn){
+  // Build the environment variable
   char *elevate_fn = malloc(strlen("ELEVATE_") + strlen(fn) + strlen("=1") + 1);
   // check if malloc failed
   if (elevate_fn == NULL) {
@@ -164,51 +183,89 @@ void config_fn(struct fn_ctrl *wr_ctrl, const char *fn){
     exit(1);
   }
 
-  // Builds elevate_fn
+  // Builds elevate_fn string "ELEVATE_fn=1"
   build_envt_var(elevate_fn, "ELEVATE_", fn, "=1");
 
+  // If environment variable 'ELEVATE_fn=1' is set, config fn_ctrl for elevate
   if(envt_var_exists(elevate_fn)){
-    printf("Found elevate envt variable\n");
-    wr_ctrl->sandwich_fn = true;
-    wr_ctrl->enter_elevated = true;
-    wr_ctrl->return_elevated = false;
+    printf("Do config for elevate\n");
+    config_fn_ctrl_for_elevate(ctrl);
   }else{
     printf("no envt variable\n");
   }
-  free(elevate_fn);
 
+  free(elevate_fn);
+}
+
+void config_fn_lower(struct fn_ctrl *ctrl, const char* fn){
+
+  // Manage lower case if necessary.
   char *lower_fn = malloc(strlen("LOWER_") + strlen(fn) + strlen("=1") + 1);
   build_envt_var(lower_fn, "LOWER_", fn, "=1");
   if(envt_var_exists(lower_fn)){
     // If already sandwiching for elevate, then we can't lower
-    assert(wr_ctrl->sandwich_fn == false);
+    assert(ctrl->sandwich_fn == false);
 
     printf("Found lower envt variable\n");
-    wr_ctrl->sandwich_fn = true;
-    wr_ctrl->enter_elevated = false;
-    wr_ctrl->return_elevated = true;
+    config_fn_ctrl_for_lower(ctrl);
   }else{
     printf("no envt variable\n");
   }
   free(lower_fn);
+}
 
-  if(envt_var_exists("SHORTCUT_write_TO_ksys_write=1")){
-    // We've got a problem if elevate and lower are set
-    assert(wr_ctrl->sandwich_fn == false);
+void config_fn_shortcut(struct fn_ctrl *ctrl, const char* fn){
+  printf("config shortcut fn is %s\n", fn);
+
+  // Manage lower case if necessary.
+  char *shortcut_fn = malloc(strlen("SHORTCUT_") + strlen(fn) + \
+                              strlen("_TO_") + strlen("ksys_") + \
+                              strlen(fn) + \
+                              strlen("=1") + 1);
+
+  build_shortcut_envt_var(shortcut_fn, "SHORTCUT_", fn, "=1");
+
+  if(envt_var_exists(shortcut_fn)){
+    // If already sandwiching for elevate, then we can't lower
+    assert(ctrl->sandwich_fn == false);
 
     printf("Found shortcut envt variable\n");
-    wr_ctrl->sandwich_fn = false;
-    wr_ctrl->do_shortcut = true;
-
+    ctrl->sandwich_fn = false;
+    ctrl->enter_elevated = false;
+    ctrl->return_elevated = false;
+    ctrl->do_shortcut = true;
+  }else{
+    printf("no envt variable\n");
   }
+
+  free(shortcut_fn);
+}
+
+
+// Sets function control flags
+void config_fn(struct fn_ctrl *ctrl, const char *fn){
+  init_fn_ctrl(ctrl);
+  // Check for elevate envt variable
+  printf("Checking for elevate envt variable\n");
+  // printf("envt var:%s\n", "ELEVATE" fn "=1");
+
+  // Manage elevate case if necessary.
+  config_fn_elevate(ctrl, fn);
+
+  // Manage lower case if necessary.
+  config_fn_lower(ctrl, fn);
+
+  // Manage shortcut case if necessary.
+  config_fn_shortcut(ctrl, fn);
+
 }
 
 // Print all fields of wr_ctrl struct
-void print_wr_ctrl(struct fn_ctrl *wr_ctrl){
-  printf("wr_ctrl->sandwich_fn: %d\n", wr_ctrl->sandwich_fn);
-  printf("wr_ctrl->enter_elevated: %d\n", wr_ctrl->enter_elevated);
-  printf("wr_ctrl->return_elevated: %d\n", wr_ctrl->return_elevated);
-  printf("wr_ctrl->do_shortcut: %d\n", wr_ctrl->do_shortcut);
+void print_ctrl(struct fn_ctrl *ctrl){
+  printf("ctrl->sandwich_fn: %d\n", ctrl->sandwich_fn);
+  printf("ctrl->enter_elevated: %d\n", ctrl->enter_elevated);
+  printf("ctrl->return_elevated: %d\n", ctrl->return_elevated);
+  printf("ctrl->do_shortcut: %d\n", ctrl->do_shortcut);
 }
 
 typedef int (*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
@@ -219,24 +276,23 @@ ssize_t write(int fd, const void* buf, size_t len){
   if (!real_write) {
 
     // Place to do one time work
-    config_fn(&wr_ctrl, __func__);
+    config_fn(&write_ctrl, __func__);
 
     // print the name of this function __func__
     real_write = dlsym(RTLD_NEXT, "write");
     printf("real_write is %p \n", real_write);
 
-    if(wr_ctrl.do_shortcut){
+    if(write_ctrl.do_shortcut){
       ksys_write = (ksys_write_t)sym_get_fn_address("ksys_write");
       // todo resolve this
       printf("warning: unconditionally elevating\n");
       sym_elevate();
     }
   }
-  // print_wr_ctrl(&wr_ctrl);
 
   // User passed -e write, we will elevate before unconditionally
-  if(wr_ctrl.sandwich_fn){
-    if(wr_ctrl.enter_elevated){
+  if(write_ctrl.sandwich_fn){
+    if(write_ctrl.enter_elevated){
     sym_elevate();
     }else{
       // User passed -l write, we will lower before unconditionally
@@ -247,7 +303,7 @@ ssize_t write(int fd, const void* buf, size_t len){
   int ret;
   // Call real write
   // todo: case for shortcut
-  if(wr_ctrl.do_shortcut){
+  if(write_ctrl.do_shortcut){
     // printf("doing shortcut\n");
     ret = ksys_write(fd, buf, len);
   }else{
@@ -257,8 +313,8 @@ ssize_t write(int fd, const void* buf, size_t len){
   }
 
   // User passed -e write, we will lower after unconditionally
-  if(wr_ctrl.sandwich_fn){
-    if(wr_ctrl.return_elevated){
+  if(write_ctrl.sandwich_fn){
+    if(write_ctrl.return_elevated){
      sym_elevate();
    }else{
     //  printf("doing lower on return path\n");
