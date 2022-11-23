db8c0c19a3930740af8945cc1fd9dedb7821e0ee
FlareCoding <albert.slepak@gmail.com>
2022-11-23 01:41:07 -0500

    made a simplified version that works with redis and the server on the redis_shortcutting branch of LinuxPrototypes

--- bin/ipc_shortcut/ipc_shortcut.c
+++ bin/ipc_shortcut/ipc_shortcut.c
@@ -7,6 +7,13 @@
 
 static JobRequestBuffer_t* s_JobBuffer = NULL;
 
+#define VERIFY_JOB_BUFFER  if (s_JobBuffer == NULL) { \
+        						s_JobBuffer = ipc_get_job_buffer(); \
+								if (s_JobBuffer == NULL) { \
+									fprintf(stderr, "[-] Failed to retrieve a working job buffer\n"); \
+								} \
+							}
+
 void __attribute__ ((constructor)) init(void) {
     printf("ipc_shortcut_lib init\n");
 }
@@ -17,42 +24,45 @@ void __attribute__ ((destructor)) cleanUp(void) {
 	}
 }
 
-int open(const char* path, int flags, ...) {
-    if (s_JobBuffer == NULL) {
-        s_JobBuffer = ipc_get_job_buffer();
-        if (s_JobBuffer == NULL) {
-            fprintf(stderr, "[-] Failed to retrieve a working job buffer\n");
-        }
-    }
+// int open(const char* path, int flags, ...) {
+    // if (s_JobBuffer == NULL) {
+    //     s_JobBuffer = ipc_get_job_buffer();
+    //     if (s_JobBuffer == NULL) {
+    //         fprintf(stderr, "[-] Failed to retrieve a working job buffer\n");
+    //     }
+    // }
 
-    s_JobBuffer->cmd = CMD_OPEN;
-	s_JobBuffer->buffer_len = strlen(path);
-	memcpy(s_JobBuffer->buffer, path, s_JobBuffer->buffer_len);
-	s_JobBuffer->arg1 = O_WRONLY | O_APPEND | O_CREAT;
+//     s_JobBuffer->cmd = CMD_OPEN;
+// 	s_JobBuffer->buffer_len = strlen(path);
+// 	memcpy(s_JobBuffer->buffer, path, s_JobBuffer->buffer_len);
+// 	s_JobBuffer->arg1 = O_WRONLY | O_APPEND | O_CREAT;
 
-	// Indicate that the job was requested
-	submit_job_request(s_JobBuffer);
+// 	// Indicate that the job was requested
+// 	submit_job_request(s_JobBuffer);
 
-	// Wait for the job to be completed
-	wait_for_job_completion(s_JobBuffer);
+// 	// Wait for the job to be completed
+// 	wait_for_job_completion(s_JobBuffer);
 
-	return s_JobBuffer->response;
-}
+// 	return s_JobBuffer->response;
+// }
 
-int close(int fd) {
-    s_JobBuffer->cmd = CMD_CLOSE;
-	s_JobBuffer->arg1 = fd;
+// int close(int fd) {
+//     s_JobBuffer->cmd = CMD_CLOSE;
+// 	s_JobBuffer->arg1 = fd;
 
-	// Indicate that the job was requested
-	submit_job_request(s_JobBuffer);
+// 	// Indicate that the job was requested
+// 	submit_job_request(s_JobBuffer);
 
-	// Wait for the job to be completed
-	wait_for_job_completion(s_JobBuffer);
+// 	// Wait for the job to be completed
+// 	wait_for_job_completion(s_JobBuffer);
 
-    return s_JobBuffer->response;
-}
+//     return s_JobBuffer->response;
+// }
 
 ssize_t write(int fd, const void* buf, size_t len) {
+	VERIFY_JOB_BUFFER
+
+	s_JobBuffer->pid = getpid();
     s_JobBuffer->cmd = CMD_WRITE;
 	s_JobBuffer->arg1 = fd;
     memcpy(s_JobBuffer->buffer, buf, len);
@@ -67,19 +77,19 @@ ssize_t write(int fd, const void* buf, size_t len) {
     return s_JobBuffer->response;
 }
 
-ssize_t read(int fd, void* buf, size_t len){
-	s_JobBuffer->cmd = CMD_READ;
-	s_JobBuffer->arg1 = fd;
-	s_JobBuffer->buffer_len = len;
+// ssize_t read(int fd, void* buf, size_t len){
+// 	s_JobBuffer->cmd = CMD_READ;
+// 	s_JobBuffer->arg1 = fd;
+// 	s_JobBuffer->buffer_len = len;
 
-	// Indicate that the job was requested
-	submit_job_request(s_JobBuffer);
+// 	// Indicate that the job was requested
+// 	submit_job_request(s_JobBuffer);
 
-	// Wait for the job to be completed
-	wait_for_job_completion(s_JobBuffer);
+// 	// Wait for the job to be completed
+// 	wait_for_job_completion(s_JobBuffer);
 
-	// Copy the shared memory read buffer to the output buffer
-	memcpy(buf, s_JobBuffer->buffer, len);
+// 	// Copy the shared memory read buffer to the output buffer
+// 	memcpy(buf, s_JobBuffer->buffer, len);
 
-    return s_JobBuffer->response;
-}
+//     return s_JobBuffer->response;
+// }
