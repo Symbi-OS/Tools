10d4e900c89030d54593b0c19c38bb60a4d8471f
FlareCoding <albert.slepak@gmail.com>
2022-11-28 12:25:57 -0500

    removed unused code

--- bin/ipc_shortcut/ipc_shortcut.c
+++ bin/ipc_shortcut/ipc_shortcut.c
@@ -24,41 +24,6 @@ void __attribute__ ((destructor)) cleanUp(void) {
 	}
 }
 
-// int open(const char* path, int flags, ...) {
-    // if (s_JobBuffer == NULL) {
-    //     s_JobBuffer = ipc_get_job_buffer();
-    //     if (s_JobBuffer == NULL) {
-    //         fprintf(stderr, "[-] Failed to retrieve a working job buffer\n");
-    //     }
-    // }
-
-//     s_JobBuffer->cmd = CMD_OPEN;
-// 	s_JobBuffer->buffer_len = strlen(path);
-// 	memcpy(s_JobBuffer->buffer, path, s_JobBuffer->buffer_len);
-// 	s_JobBuffer->arg1 = O_WRONLY | O_APPEND | O_CREAT;
-
-// 	// Indicate that the job was requested
-// 	submit_job_request(s_JobBuffer);
-
-// 	// Wait for the job to be completed
-// 	wait_for_job_completion(s_JobBuffer);
-
-// 	return s_JobBuffer->response;
-// }
-
-// int close(int fd) {
-//     s_JobBuffer->cmd = CMD_CLOSE;
-// 	s_JobBuffer->arg1 = fd;
-
-// 	// Indicate that the job was requested
-// 	submit_job_request(s_JobBuffer);
-
-// 	// Wait for the job to be completed
-// 	wait_for_job_completion(s_JobBuffer);
-
-//     return s_JobBuffer->response;
-// }
-
 ssize_t write(int fd, const void* buf, size_t len) {
 	VERIFY_JOB_BUFFER
 
@@ -76,20 +41,3 @@ ssize_t write(int fd, const void* buf, size_t len) {
 
     return s_JobBuffer->response;
 }
-
-// ssize_t read(int fd, void* buf, size_t len){
-// 	s_JobBuffer->cmd = CMD_READ;
-// 	s_JobBuffer->arg1 = fd;
-// 	s_JobBuffer->buffer_len = len;
-
-// 	// Indicate that the job was requested
-// 	submit_job_request(s_JobBuffer);
-
-// 	// Wait for the job to be completed
-// 	wait_for_job_completion(s_JobBuffer);
-
-// 	// Copy the shared memory read buffer to the output buffer
-// 	memcpy(buf, s_JobBuffer->buffer, len);
-
-//     return s_JobBuffer->response;
-// }
