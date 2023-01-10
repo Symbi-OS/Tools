1a8e916685c8bc9cc30a300812799da25c2eb770
Tommy Unger <tunger10@gmail.com>
2023-01-10 18:26:23 -0500

    Generalize interposer to any target kernel fn, add getppid

--- bin/shortcut/sc_lib.c
+++ bin/shortcut/sc_lib.c
@@ -105,7 +105,7 @@ void build_envt_var(char *buf, char *prefix, const char *fn, char *suffix) {
 
 // Only works for ksys
 void build_shortcut_envt_var(char *buf, char *prefix, const char *fn,
-                             char *suffix) {
+                             char *suffix, char *sc_target) {
 
   // Copy prefix "SHORTCUT_"
   strcpy(buf, prefix);
@@ -113,8 +113,8 @@ void build_shortcut_envt_var(char *buf, char *prefix, const char *fn,
   strcat(buf, fn);
   // Copy _TO_
   strcat(buf, "_TO_");
-  strcat(buf, "ksys_");
-  strcat(buf, fn);
+  // The kernel target
+  strcat(buf, sc_target);
   // Copy suffix
   strcat(buf, suffix);
 
@@ -177,14 +177,14 @@ void config_fn_lower(struct fn_ctrl *ctrl, const char *fn) {
   free(lower_fn);
 }
 
-void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn) {
+void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
   printf("config shortcut fn is %s\n", fn);
 
   // Manage lower case if necessary.
   char *shortcut_fn = malloc(strlen("SHORTCUT_") + strlen(fn) + strlen("_TO_") +
-                             strlen("ksys_") + strlen(fn) + strlen("=1") + 1);
+                             strlen(sc_target) + strlen("=1") + 1);
 
-  build_shortcut_envt_var(shortcut_fn, "SHORTCUT_", fn, "=1");
+  build_shortcut_envt_var(shortcut_fn, "SHORTCUT_", fn, "=1", sc_target);
 
   if (envt_var_exists(shortcut_fn)) {
     // If already sandwiching for elevate, then we can't lower
@@ -203,7 +203,7 @@ void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn) {
 }
 
 // Sets function control flags
-void config_fn(struct fn_ctrl *ctrl, const char *fn) {
+void config_fn(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
   init_fn_ctrl(ctrl);
   // Check for elevate envt variable
   printf("Checking for elevate envt variable\n");
@@ -216,7 +216,7 @@ void config_fn(struct fn_ctrl *ctrl, const char *fn) {
   config_fn_lower(ctrl, fn);
 
   // Manage shortcut case if necessary.
-  config_fn_shortcut(ctrl, fn);
+  config_fn_shortcut(ctrl, fn, sc_target);
 }
 
 // Print all fields of wr_ctrl struct
@@ -228,16 +228,17 @@ void print_ctrl(struct fn_ctrl *ctrl) {
 }
 
 // a function that gets called by get_fn_config_and_targets(ctrl, real_fn,
-// shortcut_fn, shortcut_target, __func__);
+// shortcut_fn, sc_target, __func__);
 void get_fn_config_and_targets(struct fn_ctrl *ctrl, void **real_fn,
-                               void **shortcut_fn, void *shortcut_target,
+                               void **shortcut_fn, char *sc_target,
                                const char *fn) {
-  config_fn(ctrl, fn);
+  config_fn(ctrl, fn, sc_target);
+
   // Get address of real_write
   *real_fn = dlsym(RTLD_NEXT, fn);
 
   if (ctrl->do_shortcut) {
-    *shortcut_fn = (void *)sym_get_fn_address(shortcut_target);
+    *shortcut_fn = (void *)sym_get_fn_address(sc_target);
 
     // printf("shortcut_%s is %p \n", __func__, *shortcut_fn);
 
@@ -299,5 +300,7 @@ bool do_sc(bool do_sc_for_fn) {
 }
 // This macro allocates a "real_" fn ptr, a "ksys_" fn ptr, and a fn_ctrl struct
 // Then it implements the relevant interposer fn.
-MAKE_STRUCTS_AND_FN(write, ssize_t, int, fd, const void *, buf, size_t, count)
-MAKE_STRUCTS_AND_FN(read, ssize_t, int, fd, void *, buf, size_t, count)
+MAKE_STRUCTS_AND_FN_3(write, "ksys_write", ssize_t, int, fd, const void *, buf, size_t, count)
+MAKE_STRUCTS_AND_FN_3(read, "ksys_read", ssize_t, int, fd, void *, buf, size_t, count)
+
+MAKE_STRUCTS_AND_FN_0(getppid, "__do_sys_getppid", pid_t)
