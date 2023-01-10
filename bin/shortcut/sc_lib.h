d1e2cff0ee6994017967c71547add7a8960b91ca
Tommy Unger <tunger10@gmail.com>
2023-01-10 18:26:23 -0500

    Generalize interposer to any target kernel fn, add getppid

--- bin/shortcut/sc_lib.h
+++ bin/shortcut/sc_lib.h
@@ -31,25 +31,27 @@ struct fn_ctrl {
 
 // Macro that allocates all types, variables, and structs needed
 // for each function.
+
 #define MAKE_STRUCTS(fn_name, sig) \
     typedef sig; \
     fn_name##_t  real_##fn_name = NULL; \
     struct fn_ctrl fn_name##_ctrl = {0, 0, 0, 0}; \
-    fn_name##_t ksys_##fn_name = NULL;
+    fn_name##_t sc_target_##fn_name = NULL;
 
   // If real_write is null, get it from dlsym
   // Place to do one time work
   // Configs ctrl struct with envt variables, gets ptrs to real and shortcut
-#define MAKE_INTERPOSE_FN(fn_name, ret_t, t_1, arg_1, t_2, arg_2, t_3, arg_3 ) \
+  // TODO: Could really replace __func__ with fn_name right?
+#define MAKE_INTERPOSE_FN_3(fn_name, sc_target, ret_t, t_1, arg_1, t_2, arg_2, t_3, arg_3 ) \
     ret_t fn_name ( t_1 arg_1, t_2 arg_2, t_3 arg_3 ) { \
   if (! real_##fn_name ) { \
-    get_fn_config_and_targets(& fn_name##_ctrl, (void **)&real_##fn_name, (void **) &ksys_##fn_name, \
-                              "ksys_"#fn_name, __func__); \
+    get_fn_config_and_targets(& fn_name##_ctrl, (void **)&real_##fn_name, (void **) &sc_target_##fn_name, \
+                              sc_target, __func__); \
   } \
   ingress_work(&fn_name##_ctrl); \
   int ret; \
   if ( do_sc(fn_name##_ctrl.do_shortcut) ){ \
-    ret = ksys_##fn_name( arg_1, arg_2, arg_3 ); \
+    ret = sc_target_##fn_name( arg_1, arg_2, arg_3 ); \
   } else { \
     ret = real_##fn_name( arg_1, arg_2, arg_3 ); \
   } \
@@ -57,10 +59,31 @@ struct fn_ctrl {
   return ret;\
 }
 
+#define MAKE_INTERPOSE_FN_0(fn_name, sc_target, ret_t) \
+    ret_t fn_name () { \
+  if (! real_##fn_name ) { \
+    get_fn_config_and_targets(& fn_name##_ctrl, (void **)&real_##fn_name, (void **) &sc_target_##fn_name, \
+                              sc_target, __func__); \
+  } \
+  ingress_work(&fn_name##_ctrl); \
+  int ret; \
+  if ( do_sc(fn_name##_ctrl.do_shortcut) ){ \
+    ret = sc_target_##fn_name(); \
+  } else { \
+    ret = real_##fn_name(); \
+  } \
+  egress_work(&fn_name##_ctrl); \
+  return ret;\
+}
+
 // Combine both structs, variables and fn
-#define MAKE_STRUCTS_AND_FN(fn_name, ret_t, t_1, arg_1, t_2, arg_2, t_3, arg_3 ) \
+#define MAKE_STRUCTS_AND_FN_3(fn_name, sc_target, ret_t, t_1, arg_1, t_2, arg_2, t_3, arg_3 ) \
     MAKE_STRUCTS(fn_name, ret_t (*fn_name##_t) ( t_1 arg_1, t_2 arg_2, t_3 arg_3) ) \
-    MAKE_INTERPOSE_FN(fn_name, ret_t, t_1, arg_1, t_2, arg_2, t_3, arg_3)
+    MAKE_INTERPOSE_FN_3(fn_name, sc_target, ret_t, t_1, arg_1, t_2, arg_2, t_3, arg_3)
+
+#define MAKE_STRUCTS_AND_FN_0(fn_name, sc_target, ret_t) \
+    MAKE_STRUCTS(fn_name, ret_t (*fn_name##_t) () ) \
+    MAKE_INTERPOSE_FN_0(fn_name, sc_target, ret_t)
 
 
 #endif
