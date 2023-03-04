64575ac27fbc6a4f9288f066934a2f19071ea197
Tommy Unger <tunger10@gmail.com>
2023-03-03 21:00:40 -0500

    Turn printf into fprintf, comment out syscall()

--- bin/shortcut/sc_lib.c
+++ bin/shortcut/sc_lib.c
@@ -5,59 +5,49 @@
 #include <stdlib.h>
 #include <sys/syscall.h>
 #include <unistd.h>
-
-#include "sc_lib.h"
 #include <dlfcn.h>
 #include <stdio.h>
 #include <signal.h>
-// include for var args
 #include <stdarg.h>
-
-// Include for cpu_set_t
 #include <sched.h>
-
-// TODO: Maybe move this to makefile -I?
-
 #include <sys/poll.h>
-
-// include for epoll_event
 #include <sys/epoll.h>
 
+#include "sc_lib.h"
 #ifdef DEEP_SHORTCUT
 #include "deep_sc/deep_sc.h"
 #endif
 
-// Just learned this black magic
 extern char **environ;
 
 struct cache_elem* sym_cache;
 
 // Function that prints string in red
-void print_red(const char *str) { printf("\033[1;31m%s\033[0m", str); }
+void print_red(const char *str) { fprintf(stderr, "\033[1;31m%s\033[0m", str); }
 
 // Signal handler for SIGUSR1
 // NOTE: We removed the args here int signum, dunno if that's safe
 void sigusr1_handler() {
-  printf("Received SIGUSR1\n");
-  printf("lowering now\n");
+  fprintf(stderr, "Received SIGUSR1\n");
+  fprintf(stderr, "lowering now\n");
   sym_lower();
 }
 // Signal handler for SIGUSR2
 void sigusr2_handler() {
-  printf("Received SIGUSR2\n");
-  printf("elevating now\n");
+  fprintf(stderr, "Received SIGUSR2\n");
+  fprintf(stderr, "elevating now\n");
   sym_elevate();
 }
 
 // Signal handler for SIGSYS
 void sigsys_handler() {
-  printf("Received SIGSYS\n");
+  fprintf(stderr, "Received SIGSYS\n");
 
   // Print if shortcut is on or off
   if (sc_disable) {
-    printf("Shortcut is on, turning off\n");
+    fprintf(stderr, "Shortcut is on, turning off\n");
   } else {
-    printf("Shortcut is off, turning on\n");
+    fprintf(stderr, "Shortcut is off, turning on\n");
   }
   sc_disable = !sc_disable;
 }
@@ -87,7 +77,7 @@ void __attribute__((constructor)) init(void) {
   #endif
   // function that is called when the library is loaded
   print_red("SClib: ");
-  printf("Shortcut Lib: for interposing syscalls and shortcutting\n");
+  fprintf(stderr, "Shortcut Lib: for interposing syscalls and shortcutting\n");
 
   // for debugging
   // print_sc_envt_vars();
@@ -99,7 +89,7 @@ void __attribute__((constructor)) init(void) {
 
   // If environment variable 'BEGIN_ELE=1' is set, elevate
   if (envt_var_exists("BEGIN_ELE=1")) {
-    printf("Elevating\n");
+    fprintf(stderr, "Elevating\n");
     sym_elevate();
   }
 
@@ -121,7 +111,7 @@ void build_envt_var(char *buf, char *prefix, const char *fn, char *suffix) {
   strcat(buf, fn);
   // Copy suffix
   strcat(buf, suffix);
-  // printf("elevate_fn: %s\n", buf);
+  // fprintf(stderr, "elevate_fn: %s\n", buf);
 }
 
 // Only works for ksys
@@ -139,7 +129,7 @@ void build_shortcut_envt_var(char *buf, char *prefix, const char *fn,
   // Copy suffix
   strcat(buf, suffix);
 
-  // printf("elevate_fn: %s\n", buf);
+  // fprintf(stderr, "elevate_fn: %s\n", buf);
 }
 
 void config_fn_ctrl_for_elevate(struct fn_ctrl *ctrl) {
@@ -163,7 +153,7 @@ void config_fn_elevate(struct fn_ctrl *ctrl, const char *fn) {
   char *elevate_fn = malloc(strlen("ELEVATE_") + strlen(fn) + strlen("=1") + 1);
   // check if malloc failed
   if (elevate_fn == NULL) {
-    printf("Malloc failed\n");
+    fprintf(stderr, "Malloc failed\n");
     exit(1);
   }
 
@@ -172,10 +162,10 @@ void config_fn_elevate(struct fn_ctrl *ctrl, const char *fn) {
 
   // If environment variable 'ELEVATE_fn=1' is set, config fn_ctrl for elevate
   if (envt_var_exists(elevate_fn)) {
-    printf("Do config for elevate\n");
+    fprintf(stderr, "Do config for elevate\n");
     config_fn_ctrl_for_elevate(ctrl);
   } else {
-    // printf("no envt variable\n");
+    // fprintf(stderr, "no envt variable\n");
   }
 
   free(elevate_fn);
@@ -190,16 +180,16 @@ void config_fn_lower(struct fn_ctrl *ctrl, const char *fn) {
     // If already sandwiching for elevate, then we can't lower
     assert(ctrl->sandwich_fn == false);
 
-    printf("Found lower envt variable\n");
+    fprintf(stderr, "Found lower envt variable\n");
     config_fn_ctrl_for_lower(ctrl);
   } else {
-    // printf("no envt variable\n");
+    // fprintf(stderr, "no envt variable\n");
   }
   free(lower_fn);
 }
 
 void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
-  // printf("config shortcut fn is %s\n", fn);
+  // fprintf(stderr, "config shortcut fn is %s\n", fn);
 
   // Manage lower case if necessary.
   char *shortcut_fn = malloc(strlen("SHORTCUT_") + strlen(fn) + strlen("_TO_") +
@@ -209,7 +199,7 @@ void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
 
   if (envt_var_exists(shortcut_fn)) {
     print_red("SClib: ");
-    printf("%s: requested shortcut\n", fn);
+    fprintf(stderr, "[%s]: requested shortcut\n", fn);
     // If already sandwiching for elevate, then we can't lower
     assert(ctrl->sandwich_fn == false);
 
@@ -219,7 +209,7 @@ void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
     ctrl->do_shortcut = true;
   } else {
     print_red("SClib: ");
-    printf("%s: interposed, not shortcutting\n", fn);
+    fprintf(stderr, "[%s]: interposed, not shortcutting\n", fn);
   }
 
   free(shortcut_fn);
@@ -229,8 +219,8 @@ void config_fn_shortcut(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
 void config_fn(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
   init_fn_ctrl(ctrl);
   // Check for elevate envt variable
-  // printf("Checking for elevate envt variable\n");
-  // printf("envt var:%s\n", "ELEVATE" fn "=1");
+  // fprintf(stderr, "Checking for elevate envt variable\n");
+  // fprintf(stderr, "envt var:%s\n", "ELEVATE" fn "=1");
 
   // Manage elevate case if necessary.
   config_fn_elevate(ctrl, fn);
@@ -244,10 +234,10 @@ void config_fn(struct fn_ctrl *ctrl, const char *fn, char *sc_target) {
 
 // Print all fields of wr_ctrl struct
 void print_ctrl(struct fn_ctrl *ctrl) {
-  printf("ctrl->sandwich_fn: %d\n", ctrl->sandwich_fn);
-  printf("ctrl->enter_elevated: %d\n", ctrl->enter_elevated);
-  printf("ctrl->return_elevated: %d\n", ctrl->return_elevated);
-  printf("ctrl->do_shortcut: %d\n", ctrl->do_shortcut);
+  fprintf(stderr, "ctrl->sandwich_fn: %d\n", ctrl->sandwich_fn);
+  fprintf(stderr, "ctrl->enter_elevated: %d\n", ctrl->enter_elevated);
+  fprintf(stderr, "ctrl->return_elevated: %d\n", ctrl->return_elevated);
+  fprintf(stderr, "ctrl->do_shortcut: %d\n", ctrl->do_shortcut);
 }
 
 // a function that gets called by get_fn_config_and_targets(ctrl, real_fn,
@@ -264,10 +254,10 @@ void get_fn_config_and_targets(struct fn_ctrl *ctrl, void **real_fn,
   if (ctrl->do_shortcut) {
     *shortcut_fn = (void *)sym_get_fn_address(sc_target);
 
-    // printf("shortcut_%s is %p \n", __func__, *shortcut_fn);
+    // fprintf(stderr, "shortcut_%s is %p \n", __func__, *shortcut_fn);
 
     // todo resolve this
-    printf("warning: unconditionally elevating\n");
+    fprintf(stderr, "warning: unconditionally elevating\n");
     sym_elevate();
   }
 }
@@ -280,7 +270,7 @@ void ingress_work(struct fn_ctrl *ctrl) {
       sym_elevate();
     } else {
       // User passed -l write, we will lower before unconditionally
-      // printf("doing lower on entry path\n");
+      // fprintf(stderr, "doing lower on entry path\n");
       sym_lower();
     }
   }
@@ -292,7 +282,7 @@ void egress_work(struct fn_ctrl *ctrl) {
     if (ctrl->return_elevated) {
       sym_elevate();
     } else {
-      //  printf("doing lower on return path\n");
+      //  fprintf(stderr, "doing lower on return path\n");
       sym_lower();
     }
   }
@@ -325,41 +315,15 @@ bool do_sc(bool do_sc_for_fn) {
 
 #ifndef DEEP_SHORTCUT
 
-// This macro allocates a "real_" fn ptr, a "ksys_" fn ptr, and a fn_ctrl struct
-// Then it implements the relevant interposer fn.
-
-// NOTE: Our rule for getting these fn targets is:
-// 1. Objdump vmlinux and grep for "__x64_sys_" + fn
-// 2a. if non zero args, find the fn it calls, and use that as the target
-// 2b. if zero args, use "__64_sys_" + fn as the target
 MAKE_STRUCTS_AND_FN_3(write, "__x64_sys_write", ssize_t, int, fd, const void *, buf, size_t, count)
 MAKE_STRUCTS_AND_FN_3(read, "__x64_sys_read", ssize_t, int, fd, void *, buf, size_t, count)
-
 MAKE_STRUCTS_AND_FN_0(getppid, "__x64_sys_getppid", pid_t)
 MAKE_STRUCTS_AND_FN_0(getpid, "__x64_sys_getpid", pid_t)
-
-// mmap
 MAKE_STRUCTS_AND_FN_6(mmap, "__x64_sys_mmap", void *, void *, addr, size_t, len, int, prot, int, flags, int, fd, off_t, offset)
-
-// // munmap
-// #error // target is wrong
 MAKE_STRUCTS_AND_FN_2(munmap, "__x64_sys_munmap", int, void *, addr, size_t, len)
-
-// // recvfrom
-// #error // target is wrong
-// MAKE_STRUCTS_AND_FN_6(recvfrom, "ksys_recvfrom", ssize_t, int, fd, void *, buf, size_t, len, unsigned int, flags, struct sockaddr *, src_addr, socklen_t *, addrlen)
-// // sendto
-// #error // target is wrong
-// MAKE_STRUCTS_AND_FN_6(sendto, "ksys_sendto", ssize_t, int, fd, const void *, buf, size_t, len, unsigned int, flags, const struct sockaddr *, dest_addr, socklen_t, addrlen)
-// // poll
-// #error // target is wrong
 MAKE_STRUCTS_AND_FN_3(poll, "__x64_sys_poll", int, struct pollfd *, fds, nfds_t, nfds, int, timeout)
-// // select
-// #error // target is wrong
 MAKE_STRUCTS_AND_FN_5(select, "__x64_sys_select", int, int, nfds, fd_set *, readfds, fd_set *, writefds, fd_set *, exceptfds, struct timeval *, timeout)
-
 MAKE_STRUCTS_AND_FN_4(epoll_wait, "__x64_sys_epoll_wait", int, int, epfd, struct epoll_event *, events, int, maxevents, int, timeout)
-
 MAKE_STRUCTS_AND_FN_0(fork, "__x64_sys_fork", pid_t)
 
 // fork XXX don't know if that's the right target, only need it to 
@@ -371,6 +335,7 @@ MAKE_STRUCTS_AND_FN_0(fork, "__x64_sys_fork", pid_t)
 // This will set a bit in the ctrl struct to indicate that we are coming from
 // this entry path as opposed to a glibc call like read().
 
+#if 0
 long (*real_syscall)(long, ...) = NULL;
 
 long syscall_entry_ctr = 0;
@@ -463,7 +428,7 @@ long syscall(long syscall_vec, ...) {
         arg4 = va_arg(args, uint64_t);
         arg5 = va_arg(args, uint64_t);
         arg6 = va_arg(args, uint64_t);
-        // printf("arg1: %ld arg2: %ld arg3: %ld arg4: %ld arg5: %ld arg6:
+        // fprintf(stderr, "arg1: %ld arg2: %ld arg3: %ld arg4: %ld arg5: %ld arg6:
         // %ld\n", arg1, arg2, arg3, arg4, arg5, arg6);
         return real_syscall(syscall_vec, arg1, arg2, arg3, arg4, arg5, arg6);
     }
@@ -474,14 +439,14 @@ long syscall(long syscall_vec, ...) {
     assert(false);
     return ret;
 }
-
+#endif
 #else
 
 // Type for close function
 typedef int (*close_t)(int fd);
 int close(int fd) {
   // invalidate fdth element of sym_cache array
-  /* printf("close called on fd %d\n", fd); */
+  /* fprintf(stderr, "close called on fd %d\n", fd); */
   invalidate_cache_elem(fd);
   close_t my_close = (close_t) dlsym(RTLD_NEXT, "close");
   return my_close(fd);
@@ -527,13 +492,13 @@ ssize_t write(int fd, const void *buf, size_t count) {
       ret = cached_tcp_sendmsg_path(fd, buf, count);
       // ret = real_write(fd, buf, count);
     } else {
-      /* printf("populate cache normal write path\n"); */
+      /* fprintf(stderr, "populate cache normal write path\n"); */
       ret = write_populate_cache(fd, buf, count);
       // ret = real_write(fd, buf, count);
     }
 
   } else {
-    /* printf("normal write path\n"); */
+    /* fprintf(stderr, "normal write path\n"); */
     ret = real_write(fd, buf, count);
   }
   egress_work(&write_ctrl);
