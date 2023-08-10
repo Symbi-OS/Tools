5b7b72d14017afe891872904f06bb39052a9d60c
tommy-u <tommyu@bu.edu>
2023-08-09 21:06:52 -0400

    Factor handling shortcut

--- bin/shortcut/sc_lib.h
+++ bin/shortcut/sc_lib.h
@@ -77,9 +77,12 @@ void print_args(struct pt_regs *regs) {
 
 // Todo: deal with sys vec num and floating arg
 
+
+// This saves syscall regs in the format that the kernel expects.
+// The regs struct will be the only thing passed into the syscall.
+// This assumes the syscall only needs these regs, not any others.
 // NOTE: RCX goes to r10 bc it's clobbered in syscall instruction, this is
 // what syscall() does.
-#if 1
 #define STORE_REGS()                                                       \
     asm volatile("movq %%rdi, %0" : "=m"(regs.rdi) : : "memory");          \
     asm volatile("movq %%rsi, %0" : "=m"(regs.rsi) : : "memory");          \
@@ -87,39 +90,42 @@ void print_args(struct pt_regs *regs) {
     asm volatile("movq %%rcx, %0" : "=m"(regs.r10) : : "memory");          \
     asm volatile("movq %%r8, %0" : "=m"(regs.r8) : : "memory");            \
     asm volatile("movq %%r9, %0" : "=m"(regs.r9) : : "memory");
-#else
-static inline void store_regs(struct pt_regs* regs) {
-    asm volatile("movq %%rdi, %0" : "=m"(regs->rdi) : : "memory");
-    asm volatile("movq %%rsi, %0" : "=m"(regs->rsi) : : "memory");
-    asm volatile("movq %%rdx, %0" : "=m"(regs->rdx) : : "memory");
-    asm volatile("movq %%rcx, %0" : "=m"(regs->r10) : : "memory");
-    asm volatile("movq %%r8, %0" : "=m"(regs->r8) : : "memory");
-    asm volatile("movq %%r9, %0" : "=m"(regs->r9) : : "memory");
-}
-#endif
 
-// do while here is just ensuuring macro exppands as a single statement.
+// This deals with first time initialization.
+// It may be possible to eliminate this?
 #define INITIALIZE_FUNCTION(fn_name, sc_target)                                \
         if (!real_##fn_name) {                                                 \
             get_fn_config_and_targets(                                         \
                 &fn_name##_ctrl, (void **)&real_##fn_name,                     \
                 (void **)&sc_target_##fn_name, sc_target, __func__);           \
         }
-
+// Save the user stack and switch onto the kernel stack.
+// I hate this hardcoded use of getting the kernel stack, do better.
+#define HANDLE_SHORTCUT(fn_name, sc_target, ret_t)                             \
+    uint64_t user_stack;                                                       \
+    asm volatile("mov %%rsp, %0" : "=m"(user_stack) : : "memory");             \
+    asm volatile("mov %gs:0x17b90, %rsp");                                     \
+    ret = (ret_t)MAKE_FN_CALL(sc_target_##fn_name, &regs);                     \
+    asm volatile("mov %0, %%rsp" : : "r"(user_stack));
+#if 0
+#define CALL_FUNCTION(fn_name, sc_target, ret_t, args) \
+    ret_t ret;                                         \
+    if (do_sc(fn_name##_ctrl.do_shortcut)) {           \
+        HANDLE_SHORTCUT(fn_name, sc_target, ret_t)     \
+    } else {                                           \
+        ret = MAKE_FN_CALL(real_##fn_name, args);      \
+    }
+#endif
 
 #define MAKE_INTERPOSE_FN(fn_name, sc_target, ret_t, types_and_args, args)     \
     MAKE_SIG(ret_t, fn_name, types_and_args) {                                 \
         struct pt_regs regs;                                                   \
-        STORE_REGS();                                                     \
+        STORE_REGS();                                                          \
         INITIALIZE_FUNCTION(fn_name, sc_target);                               \
         ingress_work(&fn_name##_ctrl);                                         \
         ret_t ret;                                                             \
         if (do_sc(fn_name##_ctrl.do_shortcut)) {                               \
-            uint64_t user_stack;                                               \
-            asm volatile("mov %%rsp, %0" : "=m"(user_stack) : : "memory");     \
-            asm volatile("mov %gs:0x17b90, %rsp");                           \
-            ret = (ret_t)MAKE_FN_CALL(sc_target_##fn_name, &regs);             \
-            asm volatile("mov %0, %%rsp" : : "r"(user_stack));                 \
+            HANDLE_SHORTCUT(fn_name, sc_target, ret_t)                         \
         } else {                                                               \
             ret = MAKE_FN_CALL(real_##fn_name, args);                          \
         }                                                                      \
